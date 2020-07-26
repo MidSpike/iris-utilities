cb1d0a5ba2b38ddbf7586d8f3c3283342b87a8f8
MidSpike <tylerr514@gmail.com>
2020-07-26 17:32:14 -0400

    removed uneeded stuff

--- bot.js
+++ bot.js
@@ -2,11 +2,8 @@
 require('dotenv').config();
 //#endregion Environment
 
-const fs = require('fs');
-
 const Discord = require('discord.js');
 const client = new Discord.Client({partials:['MESSAGE', 'CHANNEL', 'REACTION']});
-// client.login(JSON.parse(fs.readFileSync('./private/private-keys.json')).discord_client_token);
 client.login(process.env.BOT_TOKEN);
 
 module.exports = {
