fe7e3b0274b85e8b49c6b6e44caf6c1f900df8f1
MidSpike <developer@midspike.com>
2020-08-20 18:08:04 -0400

    2 commands now check for valid setting input

--- commands/guild_settings/set_ibm_tts_language.js
+++ commands/guild_settings/set_ibm_tts_language.js
@@ -3,6 +3,7 @@
 //#region local dependencies
 const { CustomRichEmbed } = require('../../src/CustomRichEmbed.js');
 const { DisBotCommander, DisBotCommand } = require('../../src/DisBotCommander.js');
+const ibm_languages_json = require('../../files/ibm_languages.json');
 //#endregion local dependencies
 
 module.exports = new DisBotCommand({
@@ -17,17 +18,26 @@ module.exports = new DisBotCommand({
         if (command_args[0]) {
             const old_tts_language = guild_config.tts_voice_ibm;
             const new_tts_language = command_args[0];
-            message.channel.send(new CustomRichEmbed({
-                title:`Setting New IBM TTS Language`,
-                description:`Old Server IBM TTS Language: ${'```'}\n${old_tts_language}\n${'```'}\nNew Server IBM TTS Language: ${'```'}\n${new_tts_language}\n${'```'}`
-            }, message));
-            guild_config_manipulator.modifyConfig({
-                tts_voice_ibm:new_tts_language
-            });
+            if (Object.keys(ibm_languages_json).includes(new_tts_language)) {
+                message.channel.send(new CustomRichEmbed({
+                    title:`Setting New IBM TTS Language`,
+                    description:`Old Server IBM TTS Language: ${'```'}\n${old_tts_language}\n${'```'}\nNew Server IBM TTS Language: ${'```'}\n${new_tts_language}\n${'```'}`
+                }, message));
+                guild_config_manipulator.modifyConfig({
+                    tts_voice_ibm:new_tts_language
+                });
+            } else {
+                message.channel.send(new CustomRichEmbed({
+                    color:0xFFFF00,
+                    title:`That's not a valid IBM TTS language`
+                }, message));
+            }
         } else {
-            message.channel.send(`Please provide a new ibm_tts_language after the command next time!`);
-            message.channel.send(`Example: ${'```'}\n${discord_command} en-US_EmilyV3Voice\n${'```'}`);
-            message.channel.send(`Use command \`${command_prefix}langcodes ibm\` for a list of supported voices!`);
+            message.channel.send([
+                `Please provide a new ibm_tts_language after the command next time!`,
+                `Example: ${'```'}\n${discord_command} en-US_EmilyV3Voice\n${'```'}`,
+                `Use command \`${command_prefix}langcodes ibm\` for a list of supported voices!`
+            ].join('\n'));
         }
     },
 });
