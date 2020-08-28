1111ae7cda22646d04c9e7c8544d415452ab2fb4
MidSpike <developer@midspike.com>
2020-08-28 17:00:56 -0400

    updates command description

--- commands/admin/clear.js
+++ commands/admin/clear.js
@@ -13,7 +13,7 @@ const bot_cdn_url = process.env.BOT_CDN_URL;
 module.exports = new DisBotCommand({
     name:'CLEAR',
     category:`${DisBotCommander.categories.ADMINISTRATOR}`,
-    description:'Clear / Purge messages from a channel',
+    description:'Removes a specified number of messages from a guild text-channel',
     aliases:['clear', 'purge'],
     access_level:DisBotCommand.access_levels.GUILD_MOD,
     async executor(Discord, client, message, opts={}) {
