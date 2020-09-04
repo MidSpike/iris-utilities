0cce7c0f3752a36c85d5f67970a0d9ea126a7853
MidSpike <developer@midspike.com>
2020-09-04 19:21:01 -0400

    cleanup(createConnection): prepared for future changes

--- src/libs/createConnection.js
+++ src/libs/createConnection.js
@@ -10,31 +10,47 @@ const { disBotServers } = require('../SHARED_VARIABLES.js');
  * Creates a VoiceConnection to a VoiceChannel
  * @param {VoiceChannel} voice_channel A discord.js voice channel to join
  * @param {Boolean} force_new whether or not to disconnect the bot from the voice channel before making the new connection
- * @returns {VoiceConnection|undefined}
+ * @returns {VoiceConnection|undefined} a Discord.VoiceConnection
  */
 async function createConnection(voice_channel, force_new=false) {
     if (!voice_channel) throw new Error(`voice_channel is not defined!`);
-    if (voice_channel.guild.voice?.connection && !force_new) {// Return the current voice connection
-        return voice_channel.guild.voice.connection;
-    } else {// Create a new voice connection
+
+    let voice_connection;
+
+    const current_voice_state = voice_channel.guild.voice;
+    if (current_voice_state?.connection && current_voice_state?.channel?.id === voice_channel.id && !force_new) {
+        // use the current voice_connection
+        voice_connection = current_voice_state.connection;
+    } else {
+        // create a new voice_connection
         const server = disBotServers[voice_channel.guild.id];
 
         if (force_new) server.audio_controller.disconnect();
-        await Timer(force_new ? 500 : 0);
+        await Timer(force_new ? 500 : 0); // wait a bit when force creating a new voice_connection
 
         server.queue_manager.toggleLoop(false);
         server.queue_manager.toggleAutoplay(false);
         server.queue_manager.clearItems(true);
 
-        let voice_connection;
         try {
             voice_connection = await voice_channel.join();
         } catch (error) {
-            console.trace(`Unable to join voice_channel:`, voice_channel);
-            throw new Error(`Unable to create voice connection!\n${error}`);
+            console.trace(error);
+            throw new Error(`Unable to join the voice_channel (${voice_channel.id})`);
         }
-        return voice_connection ?? undefined;
     }
+
+    /** @TODO */
+    // voice_connection?.removeAllListeners('warn');
+    // voice_connection?.on('warn', (warning) => {
+    //     console.warn(`voice_connection warning for voice_channel (${voice_connection.channel.id})`, warning);
+    // });
+    // voice_connection?.removeAllListeners('error');
+    // voice_connection?.on('error', (error) => {
+    //     console.warn(`voice_connection error for voice_channel (${voice_connection.channel.id})`, error);
+    // });
+
+    return voice_connection ?? undefined;
 }
 
 module.exports = {
