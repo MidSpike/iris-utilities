122732dce54717c8d8d161763d03ed53044705b6
MidSpike <developer@midspike.com>
2020-09-12 21:24:52 -0400

    Made `generateInviteToGuild` more useful

--- src/libs/invites.js
+++ src/libs/invites.js
@@ -11,7 +11,7 @@ const { client } = require('./bot.js');
  * @returns {Invite} an invite or throws if unsuccessful
  */
 async function generateInviteToGuild(guild_id, invite_reason='created invite via a command that was used in this server') {
-    const guild = client.guilds.cache.get(guild_id);
+    const guild = await client.guilds.fetch(guild_id);
     const invite_channel = guild.channels.cache.filter(channel => {
         const is_text_channel = channel.type === 'text';
         const can_create_invite = channel.permissionsFor(guild.me).has('CREATE_INSTANT_INVITE');
