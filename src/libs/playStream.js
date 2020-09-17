c23f95efd1e01a300ee05ea2582ab9717ee06937
MidSpike <developer@midspike.com>
2020-09-17 19:22:00 -0400

    transition(SHARED_VARIABLES)

--- src/libs/playStream.js
+++ src/libs/playStream.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { disBotServers } = require('../SHARED_VARIABLES.js');
+const { client } = require('./bot.js');
 
 //---------------------------------------------------------------------------------------------------------------//
 
@@ -12,13 +12,13 @@ const { disBotServers } = require('../SHARED_VARIABLES.js');
  * @param {Function} start_callback this callback will fire after the stream has started playing
  * @param {Function} end_callback this callback will fire after the stream has finished playing
  * @param {Function} error_callback this callback will fire when an error with the stream has occurred
- * @returns {Dispatcher} the `server.dispatcher` attached to the stream
+ * @returns {Dispatcher} the `guild_dispatcher` attached to the stream
  */
 function playStream(voice_connection, stream, volume_ratio=1.0, start_callback=(voice_connection, dispatcher)=>{}, end_callback=(voice_connection, dispatcher)=>{}, error_callback=(error)=>{}) {
     if (!voice_connection) throw new Error(`expected a 'voice_connection'`);
     if (!stream) throw new Error(`expected a 'stream'`);
 
-    const server = disBotServers[voice_connection.channel.guild.id];
+    const guild_volume_manager = client.$.volume_managers.get(voice_connection.channel.guild.id);
 
     if (typeof stream?.on === 'function') {
         stream.on('error', (error) => {
@@ -29,35 +29,36 @@ function playStream(voice_connection, stream, volume_ratio=1.0, start_callback=(
 
     const magic_volume_constant = 0.275; // this number effects all volume situations
 
-    server.dispatcher = voice_connection.play(stream, {
+    const guild_dispatcher = voice_connection.play(stream, {
         type: 'unknown',
         seek: 0,
         volume: (magic_volume_constant * volume_ratio),
         highWaterMark: 1, // set to `1` for quick volume ajustments
         fec: true,
     });
+    client.$.dispatchers.set(voice_connection.channel.guild.id, guild_dispatcher);
 
-    server.dispatcher.on('start', () => {
-        server.volume_manager.setVolume(server.volume_manager.volume);
-        start_callback(voice_connection, server.dispatcher);
+    guild_dispatcher.on('start', () => {
+        guild_volume_manager.setVolume(guild_volume_manager.volume);
+        start_callback(voice_connection, guild_dispatcher);
     });
 
-    server.dispatcher.on('finish', () => {
-        end_callback(voice_connection, server.dispatcher);
+    guild_dispatcher.on('finish', () => {
+        end_callback(voice_connection, guild_dispatcher);
     });
 
-    server.dispatcher.on('error', (error) => {
+    guild_dispatcher.on('error', (error) => {
         console.trace(error);
         error_callback(error);
     });
 
-    server.dispatcher.on('debug', (debug) => {
+    guild_dispatcher.on('debug', (debug) => {
         console.log(`----------------------------------------------------------------------------------------------------------------`);
         console.log(debug);
         console.log(`----------------------------------------------------------------------------------------------------------------`);
     });
 
-    return server.dispatcher;
+    return guild_dispatcher;
 }
 
 module.exports = {
