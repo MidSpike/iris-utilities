b496792f0ab3ce72c3ed70018b152112025b3cfb
MidSpike <developer@midspike.com>
2020-09-17 19:20:24 -0400

    transition(SHARED_VARIABLES)

--- src/commands/music/nowplaying.js
+++ src/commands/music/nowplaying.js
@@ -3,7 +3,6 @@
 //#region local dependencies
 const { CustomRichEmbed } = require('../../libs/CustomRichEmbed.js');
 const { DisBotCommander, DisBotCommand } = require('../../libs/DisBotCommander.js');
-const { disBotServers } = require('../../SHARED_VARIABLES.js');
 const { sendYtDiscordEmbed } = require('../../libs/messages.js');
 //#endregion local dependencies
 
@@ -14,23 +13,23 @@ module.exports = new DisBotCommand({
     description:'Tells the user what is currently playing',
     aliases:['nowplaying', 'np'],
     async executor(Discord, client, message, opts={}) {
-        const server = disBotServers[message.guild.id];
-        if (server.queue_manager.queue.length > 0) {
-            if (server.queue_manager.queue[0].type === 'youtube') {
-                sendYtDiscordEmbed(message, server.queue_manager.queue[0].metadata.videoInfo, 'Currently Playing');
-            } else if (server.queue_manager.queue[0].type === 'tts') {
+        const guild_queue_manager = client.$.queue_managers.get(message.guild.id);
+        if (guild_queue_manager.queue.length > 0) {
+            if (guild_queue_manager.queue[0].type === 'youtube') {
+                sendYtDiscordEmbed(message, guild_queue_manager.queue[0].metadata.videoInfo, 'Currently Playing');
+            } else if (guild_queue_manager.queue[0].type === 'tts') {
                 message.channel.send(new CustomRichEmbed({
-                    title:`Currently Playing: ${server.queue_manager.queue[0].type.toUpperCase()}`,
-                    description:`${server.queue_manager.queue[0].metadata.text.slice(0, 50)}`
+                    title:`Currently Playing: ${guild_queue_manager.queue[0].type.toUpperCase()}`,
+                    description:`${guild_queue_manager.queue[0].metadata.text.slice(0, 50)}`
                 }));
-            } else if (server.queue_manager.queue[0].type === 'mp3') {
+            } else if (guild_queue_manager.queue[0].type === 'mp3') {
                 message.channel.send(new CustomRichEmbed({
-                    title:`Currently Playing: ${server.queue_manager.queue[0].type.toUpperCase()}`,
-                    description:`${server.queue_manager.queue[0].metadata.mp3_file_name}`
+                    title:`Currently Playing: ${guild_queue_manager.queue[0].type.toUpperCase()}`,
+                    description:`${guild_queue_manager.queue[0].metadata.mp3_file_name}`
                 }));
             } else {
                 message.channel.send(new CustomRichEmbed({
-                    title:`Currently Playing: ${server.queue_manager.queue[0].type.toUpperCase()}`
+                    title:`Currently Playing: ${guild_queue_manager.queue[0].type.toUpperCase()}`
                 }));
             }
         } else {
