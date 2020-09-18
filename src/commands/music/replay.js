92c64913914a142e4e7854df60fd879cfdbe34c9
MidSpike <developer@midspike.com>
2020-09-18 09:40:07 -0400

    fixes an issue with the queue emptying

--- src/commands/music/replay.js
+++ src/commands/music/replay.js
@@ -1,6 +1,7 @@
 'use strict';
 
 //#region local dependencies
+const { createConnection } = require('../../libs/createConnection.js');
 const { CustomRichEmbed } = require('../../libs/CustomRichEmbed.js');
 const { DisBotCommander, DisBotCommand } = require('../../libs/DisBotCommander.js');
 //#endregion local dependencies
@@ -14,8 +15,19 @@ module.exports = new DisBotCommand({
     async executor(Discord, client, message, opts={}) {
         const guild_queue_manager = client.$.queue_managers.get(message.guild.id);
 
+        const voice_channel = message.member.voice.channel;
+        if (!voice_channel) {
+            message.channel.send(new CustomRichEmbed({
+                color:0xFFFF00,
+                title:`Well that's an issue!`,
+                description:`You need to be in a voice channel to use this command!`
+            }, message));
+            return;
+        }
+
         const thing_to_replay = guild_queue_manager.last_removed ?? guild_queue_manager.queue[0];
         if (thing_to_replay) {
+            await createConnection(voice_channel, false); // create a connection before adding an item to the queue
             guild_queue_manager.addItem(thing_to_replay, 1);
         } else {
             message.channel.send(new CustomRichEmbed({
