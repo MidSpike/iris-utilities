4078a38cae434885f94e593d05a875cd1a5c3011
MidSpike <developer@midspike.com>
2020-09-18 19:44:49 -0400

    feat(`flipcoin`): new alias and thumbnail results

--- src/commands/fun/flipcoin.js
+++ src/commands/fun/flipcoin.js
@@ -8,21 +8,19 @@ const { DisBotCommander, DisBotCommand } = require('../../libs/DisBotCommander.j
 module.exports = new DisBotCommand({
     name:'FLIPCOIN',
     category:`${DisBotCommander.categories.FUN}`,
-    description:'Flip a coin',
-    aliases:['flipcoin'],
+    description:'flips a virtual coin',
+    aliases:['flipcoin', 'coinflip'],
     async executor(Discord, client, message, opts={}) {
         function flipCoin() {
             const coin_chance = Math.random();
             const coin_facing = Math.round(coin_chance) === 1 ? 'heads' : 'tails';
             return { coin_chance, coin_facing };
         }
-        const { coin_chance, coin_facing } = flipCoin();
+        const { coin_facing } = flipCoin();
         message.channel.send(new CustomRichEmbed({
             title:'Flipped a coin!',
-            description:[
-                `**You got __${coin_facing}__**!`,
-                `Your chance of getting *heads* was ${(coin_chance * 100).toFixed(3)}%!`
-            ].join('\n')
+            description:`**You got __${coin_facing}__**!`,
+            thumbnail:`${process.env.BOT_CDN_URL}/Coin-${coin_facing === 'heads' ? 'H' : 'T'}_2020-09-18_b0.png`
         }, message));
     },
 });
