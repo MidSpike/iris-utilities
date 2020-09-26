2527ad314e8a0b8652e31aacdcdcd730cf9c4e72
MidSpike <developer@midspike.com>
2020-09-26 12:48:21 -0400

    cleanup and corrections

--- src/libs/GuildConfig.js
+++ src/libs/GuildConfig.js
@@ -8,44 +8,6 @@ const { object_sort } = require('../utilities.js');
 
 //---------------------------------------------------------------------------------------------------------------//
 
-const bot_guild_configs_file = path.join(process.cwd(), process.env.BOT_GUILD_CONFIGS_FILE);
-
-//---------------------------------------------------------------------------------------------------------------//
-
-/**
- * Creates an interface for accessing Guild Config Information and modifying it
- * @param {String} guild_id 
- * @returns {GuildConfigManipulator} 
- */
-class GuildConfigManipulator {
-    #configs_file = bot_guild_configs_file;
-    constructor(guild_id) {
-        this.guild_id = guild_id;
-    }
-    get configs() {
-        return JSON.parse(fs.readFileSync(this.#configs_file)) || {};
-    }
-    get config() {
-        return this.configs[this.guild_id] || {};
-    }
-    /**
-     * A non-complete object containing the guild config data changes to propagate to the config file
-     * @param {Object} new_config_data 
-     * @returns {GuildConfigManipulator} 
-     */
-    async modifyConfig(new_config_data={}) {
-        /* this will make a new config if it doesn't already exist */
-        fs.writeFileSync(this.#configs_file, JSON.stringify({
-            ...this.configs,
-            [this.guild_id]:object_sort({
-                ...this.config,
-                ...new_config_data
-            })
-        }, null, 2));
-        return this;
-    }
-}
-
 /**
  * Creates an interface for interacting with Guild Configs
  * @param {String} configs_file_relative_path a relative file path to the `.json` file from `process.cwd()`
@@ -63,36 +25,64 @@ class GuildConfigsManager {
         /* prepare the configs in memory to be stored as a `Discord.Collection` */
         this.#configs_in_memory = new Collection(configs_from_storage);
 
-        /* automatically save the configs from memory to storage every 5 minutes */
+        /* automatically save the configs from memory to storage every minute */
         setInterval(() => {
             this.saveConfigs();
-        }, 1000 * 60 * 5);
+        }, 1000 * 60 * 1);
     }
 
+    /**
+     * Retrieves all guild configs
+     * @returns {Collection<GuildId, GuildConfig>} all guild configs
+     */
     get configs() {
         return this.#configs_in_memory;
     }
 
+    /**
+     * Retrieves the config of the specified guild
+     * @param {String} guild_id 
+     * @returns {GuildConfig} a key:value guild config in the form of an object literal
+     */
     async fetchConfig(guild_id) {
         return this.#configs_in_memory.get(guild_id) ?? {};
     }
 
+    /**
+     * Updates the config of the specified guild
+     * @param {String} guild_id 
+     * @param {GuildConfig} new_config_data a key:value guild config in the form of an object literal
+     * @param {Boolean} keep_old_config_data keep existing config entries or start from scratch
+     * @returns {GuildConfigsManager} this GuildConfigsManager
+     */
     async updateConfig(guild_id, new_config_data={}, keep_old_config_data=true) {
         const old_config_data = await this.fetchConfig(guild_id);
-        return this.#configs_in_memory.set(guild_id, object_sort({
+        this.#configs_in_memory.set(guild_id, object_sort({
             ...(keep_old_config_data ? old_config_data : {}),
             ...new_config_data
         }));
+        return this;
     }
 
+    /**
+     * Removes the config of the specified guild
+     * @param {*} guild_id 
+     * @returns {GuildConfigsManager} this GuildConfigsManager
+     */
     async removeConfig(guild_id) {
-        return this.#configs_in_memory.delete(guild_id);
+        this.#configs_in_memory.delete(guild_id)
+        return this;
     }
 
+    /**
+     * Writes all of the guild configs to the guild configs file
+     * @returns {GuildConfigsManager} this GuildConfigsManager
+     */
     async saveConfigs() {
         console.time(`Saving guild configs to storage!`);
         fs.writeFileSync(this.#configs_file, JSON.stringify(Array.from(this.#configs_in_memory), null, 2));
         console.timeEnd(`Saving guild configs to storage!`);
+        return this;
     }
 }
 
