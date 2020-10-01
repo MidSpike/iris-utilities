f9a9fbe3343a21341dc3f7776021938b7c2b3154
MidSpike <developer@midspike.com>
2020-09-30 20:29:26 -0400

    cleanup

--- src/commands/info/support_discord.js
+++ src/commands/info/support_discord.js
@@ -18,13 +18,13 @@ module.exports = new DisBotCommand({
         const { discord_command } = opts;
         const support_guild = client.guilds.cache.get(bot_support_guild_id);
         const support_guild_invite = await support_guild.channels.cache.filter(c => c.type === 'text').first().createInvite({
-            unique:true,
-            maxAge:60 * 60 * 24, // 24 hours in seconds
-            reason:`@${message.author.tag} (${message.author.id}) used ${discord_command} in ${message.guild.name} ${message.guild.id}`
+            unique: true,
+            maxAge: 60 * 60 * 24, // 24 hours in seconds
+            reason: `@${message.author.tag} (${message.author.id}) used ${discord_command} in ${message.guild.name} ${message.guild.id}`,
         });
         message.channel.send(new CustomRichEmbed({
-            title:`Hey ${message.author.username}, you can speak with my support staff here!`,
-            description:`Click to join the [${support_guild.name} Discord](${support_guild_invite.url})!`
-        }, message));
+            title: `Hey ${message.author.username}, you can speak with my support staff here!`,
+            description: `Click to join the [${support_guild.name} Discord](${support_guild_invite.url})!`,
+        }, message)).catch(console.warn);
     },
 });
