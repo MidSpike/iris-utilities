4531d4ea4316ad78cf32bcf00977d8e8992e300d
MidSpike <developer@midspike.com>
2020-09-30 20:22:52 -0400

    cleanup

--- src/commands/info/info.js
+++ src/commands/info/info.js
@@ -34,59 +34,59 @@ module.exports = new DisBotCommand({
         const people_music_listeners = client.voice.connections.map(connection => connection.channel.members.filter(member => !member.user.bot).size).reduce((a, b) => a + b, 0) ?? 0;
         const bot_music_listeners = client.voice.connections.map(connection => connection.channel.members.filter(member => member.user.bot && member.user.id !== client.user.id).size).reduce((a, b) => a + b, 0) ?? 0;
         message.channel.send(new CustomRichEmbed({
-            title:`Hi There!`,
-            description:`I'm **${bot_common_name}**, the *${bot_long_name}*, a general purpose music & utility discord bot that is here to help.`,
-            fields:[
+            title: `Hi There!`,
+            description: `I\'m **${bot_common_name}**, the *${bot_long_name}*, a general purpose music & utility discord bot that is here to help.`,
+            fields: [
                 {
-                    name:'Me',
-                    value:`${bot_emoji} @${client.user.tag}`
+                    name: 'Me',
+                    value: `${bot_emoji} @${client.user.tag}`,
                 }, {
-                    name:'My Developer',
-                    value:`${midspike_emoji} @${client.users.cache.get(bot_owner_discord_id).tag}`
+                    name: 'My Developer',
+                    value: `${midspike_emoji} @${client.users.cache.get(bot_owner_discord_id).tag}`,
                 }, {
-                    name:'My Admins',
-                    value:`${super_people.filter(super_person => super_person.public).map(super_person => super_person.name).join('\n')}`
+                    name: 'My Admins',
+                    value: `${super_people.filter(super_person => super_person.public).map(super_person => super_person.name).join('\n')}`,
                 }, {
-                    name:'My Website',
-                    value:`${bot_website}`
+                    name: 'My Website',
+                    value: `${bot_website}`,
                 }, {
-                    name:'My GitHub',
-                    value:`${bot_github}`
+                    name: 'My GitHub',
+                    value: `${bot_github}`,
                 }, {
-                    name:'My Version',
-                    value:`${bot_version}`
+                    name: 'My Version',
+                    value: `${bot_version}`,
                 }, {
-                    name:'My Ping To Discord',
-                    value:`${client.ws.ping}ms`
+                    name: 'My Ping To Discord',
+                    value: `${client.ws.ping}ms`,
                 }, {
-                    name:'My Creation Date',
-                    value:`${client.user.createdAt}`
+                    name: 'My Creation Date',
+                    value: `${client.user.createdAt}`,
                 }, {
-                    name:'My Uptime',
-                    value:`${getReadableTime(client.uptime / 1000)} (hours : minutes : seconds)`
+                    name: 'My Uptime',
+                    value: `${getReadableTime(client.uptime / 1000)} (hours : minutes : seconds)`,
                 }, {
-                    name:`The Number Of People Listening To Music`,
-                    value:`${people_music_listeners} ${people_music_listeners === 1 ? 'person is' : 'people are'} listening to music`
+                    name: `The Number Of People Listening To Music`,
+                    value: `${people_music_listeners} ${people_music_listeners === 1 ? 'person is' : 'people are'} listening to music`,
                 }, {
-                    name:`The Number Of Bots Listening To Music`,
-                    value:`${bot_music_listeners} ${bot_music_listeners === 1 ? 'bot is' : 'bots are'} listening to music`
+                    name: `The Number Of Bots Listening To Music`,
+                    value: `${bot_music_listeners} ${bot_music_listeners === 1 ? 'bot is' : 'bots are'} listening to music`,
                 }, {
-                    name:`The Number Of People I Know`,
-                    value:`${client.users.cache.filter(user => !user.bot).size} People`
+                    name: `The Number Of People I Know`,
+                    value: `${client.users.cache.filter(user => !user.bot).size} People`,
                 }, {
-                    name:`The Number Of Bots I Know`,
-                    value:`${client.users.cache.filter(user => user.bot).size} Bots`
+                    name: `The Number Of Bots I Know`,
+                    value: `${client.users.cache.filter(user => user.bot).size} Bots`,
                 }, {
-                    name:`The Number Of Guilds I'm In`,
-                    value:`${client.guilds.cache.size} Guilds`
+                    name: `The Number Of Guilds I\'m In`,
+                    value: `${client.guilds.cache.size} Guilds`,
                 }, {
-                    name:'The Special Channels Usage',
-                    value:`${bot_special_text_channels.map(special_channel => `\`${special_channel.name}\` - ${client.channels.cache.filter(channel => channel.name === special_channel.name).size} Guilds`).join('\n')}`
+                    name: 'The Special Channels Usage',
+                    value: `${bot_special_text_channels.map(special_channel => `\`${special_channel.name}\` - ${client.channels.cache.filter(channel => channel.name === special_channel.name).size} Guilds`).join('\n')}`,
                 }, {
-                    name:'The Legal Disclaimer',
-                    value:`Use \`${command_prefix}disclaimer\` for information regarding your privacy and safety.`
+                    name: 'The Legal Disclaimer',
+                    value: `Use \`${command_prefix}disclaimer\` for information regarding your privacy and safety.`,
                 },
-            ]
+            ],
         }, message));
     },
 });
