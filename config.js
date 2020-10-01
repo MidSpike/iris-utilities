b45daffdbf89c60217aa5dd3a7ea8baa9fdf2c17
MidSpike <developer@midspike.com>
2020-09-30 20:29:47 -0400

    Bumped `PUBLIC_VERSION`

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const { Collection } = require('discord.js');
 
 //---------------------------------------------------------------------------------------------------------------//
 
-const PUBLIC_VERSION = 'vS_2020-09-30_0';
+const PUBLIC_VERSION = 'vS_2020-09-30_1';
 
 const SHORT_NAME = 'IRIS'; // [ a-z | A-Z | 0-9 | - | _ ]
 const COMMON_NAME = 'I.R.I.S. Utilities';
