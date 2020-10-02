0b627178730f1c3657a3520482968fbea383411d
MidSpike <developer@midspike.com>
2020-10-02 18:57:03 -0400

    feat(bot lists): Bot listing services will now be pinged every 30 minutes with updated info

---
+++ src/events/ready/bot_list_updates.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const blapi = require("blapi");
+
+const { client } = require('../../libs/bot.js');
+
+//---------------------------------------------------------------------------------------------------------------//
+
+function postStatsToBotListingServices() {
+    blapi.manualPost(client.guilds.cache.size, client.user.id, {
+        'top.gg': process.env.BLS_TOP_GG_TOKEN,
+        'bots.ondiscord.xyz': process.env.BLS_BOTS_ON_DISCORD_TOKEN,
+        'discord.bots.gg': process.env.BLS_DISCORD_BOTS_GG_TOKEN,
+        'arcane-center.xyz': process.env.BLS_ARCANE_CENTER_TOKEN,
+    });
+}
+
+module.exports = {
+    event_name: 'ready',
+    async callback() {
+        /* update the bot listing websites at the specified interval below */
+        client.setInterval(postStatsToBotListingServices, 1000 * 60 * 30); // every 30 minutes
+    }
+};
