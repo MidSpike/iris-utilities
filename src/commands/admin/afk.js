1c09e2612947ea945b0f6c1064ec664ccad557a7
MidSpike <developer@midspike.com>
2020-10-11 21:55:20 -0400

    refactored the `afk` command

--- src/commands/admin/afk.js
+++ src/commands/admin/afk.js
@@ -2,39 +2,66 @@
 
 //#region local dependencies
 const { CustomRichEmbed } = require('../../libs/CustomRichEmbed.js');
-const { DisBotCommander, DisBotCommand } = require('../../libs/DisBotCommander.js');
-const { botHasPermissionsInGuild, isThisBot, isThisBotsOwner } = require('../../libs/permissions.js');
+const { DisBotCommand,
+        DisBotCommander } = require('../../libs/DisBotCommander.js');
+const { isThisBot,
+        isThisBotsOwner,
+        botHasPermissionsInGuild } = require('../../libs/permissions.js');
 //#endregion local dependencies
 
 module.exports = new DisBotCommand({
-    name:'AFK',
-    category:`${DisBotCommander.categories.ADMINISTRATOR}`,
-    description:'Moves a user to the afk channel',
-    aliases:['afk'],
-    access_level:DisBotCommand.access_levels.GUILD_MOD,
+    name: 'AFK',
+    category: `${DisBotCommander.categories.ADMINISTRATOR}`,
+    description: 'moves a specified user to the guild\'s afk voice channel',
+    aliases: ['afk'],
+    access_level: DisBotCommand.access_levels.GUILD_MOD,
     async executor(Discord, client, message, opts={}) {
         const { command_args } = opts;
+
         if (!botHasPermissionsInGuild(message, ['MOVE_MEMBERS'])) return;
-        const user = client.users.resolve(command_args[0]) ?? message.mentions.users.first();
-        if (!user) {
+
+        const member = message.guild.members.resolve(command_args[0]) ?? message.mentions.members.first();
+        if (!member) {
             message.channel.send(new CustomRichEmbed({
-                color:0xFFFF00,
-                title:'Provide a @user next time!'
-            }, message));
+                color: 0xFFFF00,
+                title: 'Provide an @user mention next time!',
+            }, message)).catch(console.warn);
             return;
         }
+
+        if (!member.voice?.channelID) {
+            message.channel.send(new CustomRichEmbed({
+                color: 0xFFFF00,
+                title: 'The user is not in a voice channel!',
+            }, message)).catch(console.warn);
+            return;
+        }
+
         const afk_voice_channel_to_move_user_to = message.guild.afkChannelID;
         if (!afk_voice_channel_to_move_user_to) {
             message.channel.send(new CustomRichEmbed({
-                color:0xFFFF00,
-                title:`This server does not have an AFK channel!`
-            }, message));
+                color: 0xFFFF00,
+                title: 'This server does not have an AFK channel to send the user to!',
+            }, message)).catch(console.warn);
             return;
         }
-        message.guild.members.fetch(user.id).then(guildMember => {
-            if (!isThisBotsOwner(message.member.id) && isThisBotsOwner(guildMember.id)) return;
-            if (isThisBot(guildMember.id)) return;
-            guildMember.voice.setChannel(afk_voice_channel_to_move_user_to);
-        }).catch(console.trace);
+
+        /* don't allow other users to move this bot's owner */
+        if (!isThisBotsOwner(message.member.id) && isThisBotsOwner(member.id)) return;
+
+        /* don't allow users to move this bot */
+        if (isThisBot(member.id)) return;
+
+        /* move the specified guild member to the guild afk channel */
+        member.voice.setChannel(afk_voice_channel_to_move_user_to).then(() => {
+            message.channel.send(new CustomRichEmbed({
+                description: `Moved ${member} to this guild\'s AFK voice channel.`,
+            }, message)).catch(console.warn);
+        }).catch(() => {
+            message.channel.send(new CustomRichEmbed({
+                color: 0xFFFF00,
+                description: `I was unable to move ${member} to this guild\'s AFK voice channel.`,
+            }, message)).catch(console.warn);
+        });
     },
 });
