eb157c16575682e8e279f786fa62347e5149b795
MidSpike <developer@midspike.com>
2020-10-12 19:50:26 -0400

    refactored the `move` command

--- src/commands/admin/move.js
+++ src/commands/admin/move.js
@@ -2,39 +2,79 @@
 
 //#region local dependencies
 const { CustomRichEmbed } = require('../../libs/CustomRichEmbed.js');
-const { DisBotCommander, DisBotCommand } = require('../../libs/DisBotCommander.js');
-const { botHasPermissionsInGuild, isThisBot, isThisBotsOwner } = require('../../libs/permissions.js');
+const { DisBotCommand,
+        DisBotCommander } = require('../../libs/DisBotCommander.js');
+const { isThisBot,
+        isThisBotsOwner,
+        botHasPermissionsInGuild } = require('../../libs/permissions.js');
 //#endregion local dependencies
 
 module.exports = new DisBotCommand({
-    name:'MOVE',
-    category:`${DisBotCommander.categories.ADMINISTRATOR}`,
-    description:'Move a user to a specified channel',
-    aliases:['move'],
-    access_level:DisBotCommand.access_levels.GUILD_MOD,
+    name: 'MOVE',
+    category: `${DisBotCommander.categories.ADMINISTRATOR}`,
+    description: 'moves a specified user to a specified voice channel',
+    aliases: ['move'],
+    access_level: DisBotCommand.access_levels.GUILD_MOD,
     async executor(Discord, client, message, opts={}) {
+        /**
+         * Example Command Usage:
+         * command_name member_id_or_mention voice_channel_id
+         */
+
         const { command_args } = opts;
+
         if (!botHasPermissionsInGuild(message, ['MOVE_MEMBERS'])) return;
-        const user = client.users.resolve(command_args[0]) ?? message.mentions.users.first();
-        if (!user) {
+
+        const member = message.guild.members.resolve(command_args[0]) ?? message.mentions.members.first();
+        if (!member) {
             message.channel.send(new CustomRichEmbed({
-                color:0xFFFF00,
-                title:'Provide a @user next time!'
+                color: 0xFFFF00,
+                title: 'Provide an @user mention next time!',
+            }, message)).catch(console.warn);
+            return;
+        }
+
+        if (!member.voice?.channelID) {
+            message.channel.send(new CustomRichEmbed({
+                color: 0xFFFF00,
+                title: 'That user is not in a voice channel!',
+            }, message)).catch(console.warn);
+            return;
+        }
+
+        const voice_channel_id = command_args[1];
+        if (!voice_channel_id) {
+            message.channel.send(new CustomRichEmbed({
+                color: 0xFFFF00,
+                title: 'You must specify a voice channel id after the user mention!',
             }, message));
             return;
         }
-        const voice_channel_to_move_user_to = message.guild.channels.cache.filter(c => c.type === 'voice').get(command_args[1]);
-        if (!voice_channel_to_move_user_to) {
+
+        const voice_channel = message.guild.channels.cache.filter(channel => channel.type === 'voice').get(voice_channel_id);
+        if (!voice_channel) {
             message.channel.send(new CustomRichEmbed({
-                color:0xFFFF00,
-                title:`That voice channel doesn't exist!`
+                color: 0xFFFF00,
+                title: 'That voice channel doesn\'t exist!',
             }, message));
             return;
         }
-        message.guild.members.fetch(user.id).then(guildMember => {
-            if (!isThisBotsOwner(message.member.id) && isThisBotsOwner(guildMember.id)) {return;}
-            if (isThisBot(guildMember.id)) {return;}
-            guildMember.voice.setChannel(voice_channel_to_move_user_to);
-        }).catch(console.trace);
+
+        /* don't allow other users to move this bot's owner */
+        if (!isThisBotsOwner(message.member.id) && isThisBotsOwner(member.id)) return;
+
+        /* don't allow users to move this bot */
+        if (isThisBot(member.id)) return;
+
+        member.voice.setChannel(voice_channel).then(() => {
+            message.channel.send(new CustomRichEmbed({
+                description: `Moved ${member} to ${voice_channel.name}.`,
+            }, message)).catch(console.warn);
+        }).catch(() => {
+            message.channel.send(new CustomRichEmbed({
+                color: 0xFFFF00,
+                description: `I was unable to move ${member} to ${voice_channel.name}.`,
+            }, message)).catch(console.warn);
+        });
     },
 });
