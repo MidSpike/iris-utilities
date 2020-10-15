032b0ae589b39d1980248e4e08f007707e8f5994
MidSpike <developer@midspike.com>
2020-10-15 19:30:03 -0400

    continued work on the `memberlist` command

--- src/commands/hidden/memberlist.js
+++ src/commands/hidden/memberlist.js
@@ -30,7 +30,7 @@ module.exports = new DisBotCommand({
                 })),
             }));
     
-            const temp_file_path = path.join(process.cwd(), 'temporary', `Member-List_${Date.now()}.json`);
+            const temp_file_path = path.join(process.cwd(), 'temporary', `Member-List_${message.guild.id}_${Date.now()}.json`);
             fs.writeFileSync(temp_file_path, JSON.stringify(mapped_members, null, 2), { flag: 'w' });
     
             const temp_file_read_stream = fs.createReadStream(temp_file_path);
@@ -44,10 +44,11 @@ module.exports = new DisBotCommand({
         const embed = new CustomRichEmbed({
             title: 'Do you want to receive the member-list file here?',
             description: [
+                'This command creates a \`.json\` file containing a list of all members in this guild along with their roles.\n',
                 'If you say yes, the file will be sent to this channel.',
                 'If you say no, the file will be directly messaged to you.',
             ].join('\n'),
-        });
+        }, message);
 
         sendConfirmationEmbed(message.author.id, message.channel.id, true, embed, async () => {
             sendMemberListFileToChannel(message.channel);
