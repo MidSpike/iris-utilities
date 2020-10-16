dc52d861e3657fd5f37805cc70ac101873693ec7
MidSpike <developer@midspike.com>
2020-10-15 21:44:49 -0400

    started refactor of `takerole` command

--- src/commands/admin/takerole.js
+++ src/commands/admin/takerole.js
@@ -7,34 +7,43 @@ const { botHasPermissionsInGuild } = require('../../libs/permissions.js');
 //#endregion local dependencies
 
 module.exports = new DisBotCommand({
-    name:'TAKEROLE',
-    category:`${DisBotCommander.categories.ADMINISTRATOR}`,
-    description:'Takes a role',
-    aliases:['takerole'],
-    access_level:DisBotCommand.access_levels.GUILD_ADMIN,
+    name: 'TAKEROLE',
+    category: `${DisBotCommander.categories.ADMINISTRATOR}`,
+    description: 'takes a specified role from a specified member',
+    aliases: ['takerole'],
+    access_level: DisBotCommand.access_levels.GUILD_ADMIN,
     async executor(Discord, client, message, opts={}) {
         const { discord_command, command_args } = opts;
+
         if (!botHasPermissionsInGuild(message, ['MANAGE_ROLES'])) return;
-        const user = client.users.resolve(command_args[0]) ?? message.mentions.users.first();
-        const guildMember = await message.guild.members.fetch(user);
-        const role_to_remove = message.guild.roles.cache.get(command_args[1]) ?? message.mentions.roles.first();
-        if (guildMember && role_to_remove) {
-            const user_is_greater_than_member = message.member.roles.highest.comparePositionTo(guildMember.roles.highest) > 0; // DO NOT TOUCH
-            const user_is_greater_than_role_to_remove = message.member.roles.highest.comparePositionTo(role_to_remove) >= 0; // DO NOT TOUCH
-            if (user_is_greater_than_member && user_is_greater_than_role_to_remove) {
-                guildMember.roles.remove(role_to_remove).then(guildMember => {
+
+        const member = message.guild.members.cache.get(command_args[0]) ?? message.mentions.members.first();
+        const role = message.guild.roles.cache.get(command_args[1]) ?? message.mentions.roles.first();
+
+        if (member && role) {
+            const staff_highest_role_is_greater_than_member_highest_role = message.member.roles.highest.comparePositionTo(member.roles.highest) > 0;
+            const staff_highest_role_is_greater_than_role = message.member.roles.highest.comparePositionTo(role) > 0;
+
+            if (staff_highest_role_is_greater_than_member_highest_role && staff_highest_role_is_greater_than_role) {
+                member.roles.remove(role).then(() => {
                     message.channel.send(new CustomRichEmbed({
-                        title:`Role Manager`,
-                        description:`Removed ${role_to_remove} from ${guildMember.user.tag}`
-                    }, message));
-                }).catch(console.warn);
+                        title: 'Role Manager',
+                        description: `Removed ${role.name} from ${member}!`,
+                    }, message)).catch(console.warn);
+                }).catch((error) => {
+                    logUserError(message, error);
+                });
             } else {
-                message.channel.send(`Something fishy is going on here!`);
-                message.channel.send(`You aren't qualified to take this role from them.`);
+                message.channel.send([
+                    'Something fishy is going on here!',
+                    'You aren\'t allowed to take this role from that user.',
+                ].join('\n'));
             }
         } else {
-            message.channel.send('Please specify a user and role to take');
-            message.channel.send(`Example: ${'```'}\n${discord_command} @user#0001 ROLE_ID_HERE${'```'}`);
+            message.channel.send([
+                'Please specify a user and a role for this command!',
+                `Example: ${'```'}\n${discord_command} @user#0001 ROLE_HERE\n${'```'}`,
+            ].join('\n'));
         }
     },
 });
