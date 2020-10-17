0ca41e07cc71c50f509dcb245da6a0da856b8b07
MidSpike <developer@midspike.com>
2020-10-16 20:47:53 -0400

    cleanup

--- src/commands/hidden/test.js
+++ src/commands/hidden/test.js
@@ -7,11 +7,11 @@ const { DisBotCommand,
 //#endregion local dependencies
 
 module.exports = new DisBotCommand({
-    name:'TEST',
-    category:`${DisBotCommander.categories.HIDDEN}`,
-    description:'used for testing purposes',
-    aliases:['test'],
-    access_level:DisBotCommand.access_levels.BOT_SUPER,
+    name: 'TEST',
+    category: `${DisBotCommander.categories.HIDDEN}`,
+    description: 'used for testing purposes',
+    aliases: ['test'],
+    access_level: DisBotCommand.access_levels.BOT_SUPER,
     async executor(Discord, client, message, opts={}) {
         message.channel.send(new CustomRichEmbed({
             title: `Hello World! I'm ${client.user.username}!`,
