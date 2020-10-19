8e49596aa49ee42dcbd6968ee68fd9b36fe6d098
MidSpike <developer@midspike.com>
2020-10-18 20:47:33 -0400

    bug fixes for `tti`

--- src/commands/utilities/text_to_image.js
+++ src/commands/utilities/text_to_image.js
@@ -23,7 +23,25 @@ module.exports = new DisBotCommand({
     async executor(Discord, client, message, opts={}) {
         const { discord_command } = opts;
 
-        const text_for_image = escapeHTML(message.cleanContent.replace(discord_command, '')).trim().replace(/\r?\n|\r/g, '<br />');
+        const user_raw_args_start_end_regex = /(format(\s*?)\{|\})/i;
+        const user_raw_args_regex = /(format(\s*?)\{(.|\s)*?\})/i;
+        const user_raw_args = message.cleanContent.match(user_raw_args_regex)?.[0] ?? '';
+        console.log({user_raw_args});
+
+        const user_args_invalid_regex = /([^a-z0-9\#\_\-])/gi;
+        
+        const user_args = user_raw_args.replace(user_raw_args_start_end_regex, '').trim().split(';').map(kv => 
+            kv.split(':').map(i => 
+                i.replace(user_args_invalid_regex, '').trim()
+            )
+        );
+        console.log({user_args});
+        
+        const user_args_map = new Map(user_args);
+        console.log({user_args_map});
+
+        let text_for_image = message.cleanContent.replace(user_raw_args_regex, '').trim();
+        text_for_image = escapeHTML(text_for_image.replace(discord_command, '')).trim().replace(/\r?\n|\r/g, '<br />');
 
         if (text_for_image.length === 0) {
             message.channel.send(new CustomRichEmbed({
@@ -32,8 +50,11 @@ module.exports = new DisBotCommand({
                 description: 'This command can create images from the text that you specify.',
                 fields: [
                     {
-                        name: 'Text-To-Image Command Usage',
+                        name: 'Text-To-Image Simple Usage',
                         value: `${'```'}\n${discord_command} Hello World!\n${'```'}`,
+                    }, {
+                        name: 'Text-To-Image Advanced Usage',
+                        value: `${'```'}\n${discord_command} format{\n bg-color: black;\n text-color: #FF5500;\n text-size: 24px;\n}\nHello World!\n${'```'}`,
                     },
                 ],
             }, message));
@@ -42,7 +63,7 @@ module.exports = new DisBotCommand({
 
         const image = await nodeHtmlToImage({
             type: 'png',
-            encoding: 'binary', // binary | base64
+            encoding: 'binary',
             html: (`
                 <!DOCTYPE html>
                 <html lang="en">
@@ -50,35 +71,35 @@ module.exports = new DisBotCommand({
                         <meta charset="UTF-8" />
                         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                         <style>
-                            /* @import url('https://fonts.googleapis.com/css2?family=Domine:wght@500&display=swap'); */
                             @import url('https://fonts.googleapis.com/css2?family=Inconsola:wght@900&display=swap');
                             * {
                                 box-sizing: border-box;
                             }
                             html {
+                                display: flex;
                                 width: auto;
                                 height: auto;
                                 max-width: 500px;
                                 overflow: hidden;
-                                word-wrap: break-word;
                             }
                             body {
                                 width: auto;
                                 height: auto;
                                 padding: 10px 15px;
-                                background: #7289da;
-                                color: #ffffff;
-                                font-size: 20px;
+                                background: ${user_args_map.get('bg-color') ?? '#7289da'};
+                                color: ${user_args_map.get('text-color') ?? '#ffffff'};
+                                font-size: ${user_args_map.get('text-size') ?? '20px'};
                                 font-weight: 900;
-                                /* font-family: 'Domine', serif; */
                                 font-family: 'Inconsola', monospace;
+                            }
+                            div {
+                                word-wrap: break-word;
+                                white-space: pre;
                             }
                         </style>
                     </head>
                     <body>
-                        <div>
-                            ${text_for_image}
-                        </div>
+                        <div>${text_for_image}</div>
                     </body>
                 </html>
             `),
