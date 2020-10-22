bbb16d951c2565f7b1168ad9599c9fb87fb8847c
MidSpike <developer@midspike.com>
2020-10-22 15:50:46 -0400

    Polished the `memberlist` command

--- src/commands/hidden/memberlist.js
+++ src/commands/hidden/memberlist.js
@@ -13,7 +13,7 @@ const { DisBotCommand,
 module.exports = new DisBotCommand({
     name: 'MEMBERLIST',
     category: `${DisBotCommander.categories.HIDDEN}`,
-    description: 'allows you to download the member list for servers that you own',
+    description: 'allows you to download the member-list for servers that you own',
     aliases: ['memberlist'],
     access_level: DisBotCommand.access_levels.GUILD_OWNER,
     async executor(Discord, client, message, opts={}) {
@@ -30,28 +30,28 @@ module.exports = new DisBotCommand({
                 })),
             }));
     
-            const temp_file_path = path.join(process.cwd(), 'temporary', `Member-List_${message.guild.id}_${Date.now()}.json`);
+            const temp_file_path = path.join(process.cwd(), 'temporary', `member-list_${message.guild.id}_${Date.now()}.json`);
             fs.writeFileSync(temp_file_path, JSON.stringify(mapped_members, null, 2), { flag: 'w' });
     
             const temp_file_read_stream = fs.createReadStream(temp_file_path);
             const message_attachment = new Discord.MessageAttachment(temp_file_read_stream);
 
-            await channel.send(`${message.author} here is the member list for ${message.guild.name} (${message.guild.id})`, message_attachment).catch(console.warn);
+            await channel.send(`${message.author} here is the member-list for ${message.guild.name} (${message.guild.id})`, message_attachment).catch(console.warn);
 
             fs.unlinkSync(temp_file_path);
         }
 
         const embed = new CustomRichEmbed({
-            title: 'Do you want to receive the member-list file (for this server) here?',
             description: [
-                'This command creates a \`.json\` file containing a list of all members in this guild along with their roles.',
-                '\`.json\` files can be viewed as a plain-text file in most text editing programs.',
-                'You can also use a code editor such as VS Code, Notepad++, Atom, etc.',
-                '\n',
-                'It is recommended that you limit the amount of people that are allowed to see this file.',
-                '\n',
-                'If you say yes, the file will be sent to this channel.',
-                'If you say no, the file will be directly messaged to you.',
+                'This command creates a \`.json\` file containing all of the members in this guild along with their roles.',
+                '\n\`.json\` files can be viewed as a plain-text file in most text-editing programs.',
+                'You can also use a code-editor such as [VS Code](https://code.visualstudio.com/), [Notepad++](https://notepad-plus-plus.org/), or [Atom](https://atom.io/).',
+                '\nIf \`.json\` files are a bit un-intuitive to you; I suggest converting it to a \`.csv\` file.',
+                'You can open \`.csv\` files with [Microsoft Excel](https://www.office.com/launch/excel) or [Google Sheets](https://docs.google.com/spreadsheets/).',
+                'You can convert the \`.json\` file to \`.csv\` using [json-csv](https://json-csv.com/)',
+                '\n**Do you want to receive the member-list for this guild, here?**',
+                'If you say **yes**, the file will be sent to this channel.',
+                'If you say **no**, the file will be directly messaged to you.',
             ].join('\n'),
         }, message);
 
