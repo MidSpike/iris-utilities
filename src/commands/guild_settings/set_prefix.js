00b2f85eba3d7a51d6812744a94419b81dea9710
MidSpike <developer@midspike.com>
2020-10-26 16:22:21 -0400

    cleanup

--- src/commands/guild_settings/set_prefix.js
+++ src/commands/guild_settings/set_prefix.js
@@ -1,21 +1,20 @@
 'use strict';
 
-//#region local dependencies
+//#region dependencies
 const { CustomRichEmbed } = require('../../libs/CustomRichEmbed.js');
-const { DisBotCommander, DisBotCommand } = require('../../libs/DisBotCommander.js');
-//#endregion local dependencies
+const { DisBotCommand,
+        DisBotCommander } = require('../../libs/DisBotCommander.js');
+//#endregion dependencies
 
 module.exports = new DisBotCommand({
-    name:'SET_PREFIX',
-    category:`${DisBotCommander.categories.GUILD_SETTINGS}`,
-    weight:2,
-    description:'sets prefix',
-    aliases:['set_prefix'],
-    access_level:DisBotCommand.access_levels.GUILD_ADMIN,
+    name: 'SET_PREFIX',
+    category: `${DisBotCommander.categories.GUILD_SETTINGS}`,
+    weight: 2,
+    description: 'sets the command prefix for this bot',
+    aliases: ['set_prefix'],
+    access_level: DisBotCommand.access_levels.GUILD_ADMIN,
     async executor(Discord, client, message, opts={}) {
         const { command_prefix, command_args, discord_command } = opts;
-
-        const guild_config = await client.$.guild_configs_manager.fetchConfig(message.guild.id);
 
         if (command_args[0]) {
             if (message.mentions.users.size > 0) {
@@ -26,23 +25,38 @@ module.exports = new DisBotCommand({
                 }, message));
                 return;
             }
+
+            const guild_config = await client.$.guild_configs_manager.fetchConfig(message.guild.id);
+            
             const old_command_prefix = guild_config.command_prefix;
-            const new_command_prefix = command_args[0].replace(/\s/g, '_').toLowerCase(); // Replace whitespaces with underscores
+            const new_command_prefix = command_args[0].trim().replace(/\s/g, '_').toLowerCase(); // replace whitespaces with underscores and convert to lowercase
+
             message.channel.send(new CustomRichEmbed({
-                title:'Setting New Command Prefix',
-                description:`Old Server Command Prefix: ${'```'}\n${old_command_prefix}\n${'```'}\nNew Server Command Prefix: ${'```'}\n${new_command_prefix}\n${'```'}`
+                title: 'Setting New Command Prefix',
+                description: [
+                    `Old Guild Command Prefix:`,
+                    `${'```'}\n${old_command_prefix}\n${'```'}`,
+                    `New Guild Command Prefix:`,
+                    `${'```'}\n${new_command_prefix}\n${'```'}`,
+                ].join('\n'),
             }, message));
+
             client.$.guild_configs_manager.updateConfig(message.guild.id, {
-                command_prefix:new_command_prefix
+                command_prefix: new_command_prefix,
             });
         } else {
             message.channel.send(new CustomRichEmbed({
-                title:`Well I guess it's time for me to respond to something new!`,
-                description:'Make sure to enter a new command_prefix after the command next time!',
-                fields:[
-                    {name:'Example Command Usage', value:`${'```'}\n${discord_command} $\n${'```'}`},
-                    {name:'Example Description', value:`If you run the above command, I will start responding to commands using \`$\` instead of \`${command_prefix}\``},
-                ]
+                title: `Well I guess it's time for me to respond to something new!`,
+                description: 'Make sure to enter a new command_prefix after the command next time!',
+                fields: [
+                    {
+                        name: 'Example Command Usage',
+                        value: `${'```'}\n${discord_command} $\n${'```'}`
+                    }, {
+                        name: 'Example Description',
+                        value: `If you run the above command, I will start responding to commands using \`$\` instead of \`${command_prefix}\``
+                    },
+                ],
             }));
         }
     },
