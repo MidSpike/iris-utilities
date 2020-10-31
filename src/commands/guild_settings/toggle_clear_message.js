42c534e875319d00812f9ee0d8adcc09da1ae0cb
MidSpike <developer@midspike.com>
2020-10-31 18:23:49 -0400

    cleanup

--- src/commands/guild_settings/toggle_clear_message.js
+++ src/commands/guild_settings/toggle_clear_message.js
@@ -2,16 +2,17 @@
 
 //#region local dependencies
 const { CustomRichEmbed } = require('../../libs/CustomRichEmbed.js');
-const { DisBotCommander, DisBotCommand } = require('../../libs/DisBotCommander.js');
+const { DisBotCommand,
+        DisBotCommander } = require('../../libs/DisBotCommander.js');
 //#endregion local dependencies
 
 module.exports = new DisBotCommand({
-    name:'TOGGLE_CLEAR_MESSAGE',
-    category:`${DisBotCommander.categories.GUILD_SETTINGS}`,
-    weight:15,
-    description:'toggles clear message',
-    aliases:['toggle_clear_message'],
-    access_level:DisBotCommand.access_levels.GUILD_ADMIN,
+    name: 'TOGGLE_CLEAR_MESSAGE',
+    category: `${DisBotCommander.categories.GUILD_SETTINGS}`,
+    weight: 15,
+    description: 'toggles clear message',
+    aliases: ['toggle_clear_message'],
+    access_level: DisBotCommand.access_levels.GUILD_ADMIN,
     async executor(Discord, client, message, opts={}) {
         const { command_prefix } = opts;
 
@@ -20,19 +21,19 @@ module.exports = new DisBotCommand({
 
         if (clear_message === true) {
             message.channel.send(new CustomRichEmbed({
-                title:`Clear Message: disabled;`,
-                description:`\`${command_prefix}clear\` will not say when it has been used.`
+                title: 'Clear Message: disabled;',
+                description: `\`${command_prefix}clear\` will not say when it has been used.`,
             }, message));
             client.$.guild_configs_manager.updateConfig(message.guild.id, {
-                clear_message:'disabled'
+                clear_message: 'disabled',
             });
         } else {
             message.channel.send(new CustomRichEmbed({
-                title:`Clear Message: enabled;`,
-                description:`\`${command_prefix}clear\` will say when it has been used.`
+                title: 'Clear Message: enabled;',
+                description: `\`${command_prefix}clear\` will say when it has been used.`,
             }, message));
             client.$.guild_configs_manager.updateConfig(message.guild.id, {
-                clear_message:'enabled'
+                clear_message: 'enabled',
             });
         }
     },
