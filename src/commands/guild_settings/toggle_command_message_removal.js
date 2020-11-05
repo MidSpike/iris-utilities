30ebe0999cfe3a72d58f5249fcfe1ee144fad840
MidSpike <developer@midspike.com>
2020-11-04 20:29:28 -0500

    cleanup

--- src/commands/guild_settings/toggle_command_message_removal.js
+++ src/commands/guild_settings/toggle_command_message_removal.js
@@ -2,34 +2,35 @@
 
 //#region local dependencies
 const { CustomRichEmbed } = require('../../libs/CustomRichEmbed.js');
-const { DisBotCommander, DisBotCommand } = require('../../libs/DisBotCommander.js');
+const { DisBotCommand,
+        DisBotCommander } = require('../../libs/DisBotCommander.js');
 //#endregion local dependencies
 
 module.exports = new DisBotCommand({
-    name:'TOGGLE_COMMAND_MESSAGE_REMOVAL',
-    category:`${DisBotCommander.categories.GUILD_SETTINGS}`,
-    weight:12,
-    description:'toggles command message removal',
-    aliases:['toggle_command_message_removal'],
-    access_level:DisBotCommand.access_levels.GUILD_ADMIN,
+    name: 'TOGGLE_COMMAND_MESSAGE_REMOVAL',
+    category: `${DisBotCommander.categories.GUILD_SETTINGS}`,
+    weight: 12,
+    description: 'toggles removal of command messages',
+    aliases: ['toggle_command_message_removal'],
+    access_level: DisBotCommand.access_levels.GUILD_ADMIN,
     async executor(Discord, client, message, opts={}) {
         const guild_config = await client.$.guild_configs_manager.fetchConfig(message.guild.id);
         const command_message_removal = guild_config.command_message_removal === 'enabled';
         if (command_message_removal === true) {
             message.channel.send(new CustomRichEmbed({
-                title:`Command Message Removal: disabled;`,
-                description:`When a user uses a command, the user's message will not be removed.`
+                title: 'Command Message Removal: disabled;',
+                description: 'When a user uses a command, the user\'s message will not be removed.',
             }, message));
             client.$.guild_configs_manager.updateConfig(message.guild.id, {
-                command_message_removal:'disabled'
+                command_message_removal: 'disabled',
             });
         } else {
             message.channel.send(new CustomRichEmbed({
-                title:`Command Message Removal: enabled;`,
-                description:`When a user uses a command, the user's message will be removed.`
+                title: 'Command Message Removal: enabled;',
+                description: 'When a user uses a command, the user\'s message will be removed.',
             }, message));
             client.$.guild_configs_manager.updateConfig(message.guild.id, {
-                command_message_removal:'enabled'
+                command_message_removal: 'enabled',
             });
         }
     },
