ed77ebaab09cb6f3cb2dc7c4ab2d42bf2f66d114
MidSpike <developer@midspike.com>
2020-11-06 19:57:05 -0500

    cleanup

--- src/commands/guild_settings/toggle_invite_blocking.js
+++ src/commands/guild_settings/toggle_invite_blocking.js
@@ -2,7 +2,7 @@
 
 //#region local dependencies
 const { CustomRichEmbed } = require('../../libs/CustomRichEmbed.js');
-const { DisBotCommander, DisBotCommand } = require('../../libs/DisBotCommander.js');
+const { DisBotCommand, DisBotCommander } = require('../../libs/DisBotCommander.js');
 //#endregion local dependencies
 
 module.exports = new DisBotCommand({
