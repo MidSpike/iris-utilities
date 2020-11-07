49bc0de1ec9cfff4b8c977afdf25e769599042f1
MidSpike <developer@midspike.com>
2020-11-06 19:54:15 -0500

    cleanup

--- src/commands/guild_settings/toggle_url_blocking.js
+++ src/commands/guild_settings/toggle_url_blocking.js
@@ -2,34 +2,35 @@
 
 //#region local dependencies
 const { CustomRichEmbed } = require('../../libs/CustomRichEmbed.js');
-const { DisBotCommander, DisBotCommand } = require('../../libs/DisBotCommander.js');
+const { DisBotCommand,
+        DisBotCommander } = require('../../libs/DisBotCommander.js');
 //#endregion local dependencies
 
 module.exports = new DisBotCommand({
-    name:'TOGGLE_URL_BLOCKING',
-    category:`${DisBotCommander.categories.GUILD_SETTINGS}`,
-    weight:16,
-    description:'toggles url blocking',
-    aliases:['toggle_url_blocking'],
-    access_level:DisBotCommand.access_levels.GUILD_ADMIN,
+    name: 'TOGGLE_URL_BLOCKING',
+    category: `${DisBotCommander.categories.GUILD_SETTINGS}`,
+    weight: 16,
+    description: 'toggles url blocking for messages sent by users',
+    aliases: ['toggle_url_blocking'],
+    access_level: DisBotCommand.access_levels.GUILD_ADMIN,
     async executor(Discord, client, message, opts={}) {
         const guild_config = await client.$.guild_configs_manager.fetchConfig(message.guild.id);
         const url_blocking = guild_config.url_blocking === 'enabled';
         if (url_blocking === true) {
             message.channel.send(new CustomRichEmbed({
-                title:`URL Blocking: disabled;`,
-                description:`URLs sent by members sent in the server will not be automatically deleted.`
+                title: 'URL Blocking: disabled;',
+                description: 'URLs sent by members sent in the server will not be automatically deleted.',
             }, message));
             client.$.guild_configs_manager.updateConfig(message.guild.id, {
-                url_blocking:'disabled'
+                url_blocking: 'disabled',
             });
         } else {
             message.channel.send(new CustomRichEmbed({
-                title:`URL Blocking: enabled;`,
-                description:`URLs sent by members sent in the server will be automatically deleted.`
+                title: 'URL Blocking: enabled;',
+                description: 'URLs sent by members sent in the server will be automatically deleted.',
             }, message));
             client.$.guild_configs_manager.updateConfig(message.guild.id, {
-                url_blocking:'enabled'
+                url_blocking: 'enabled',
             });
         }
     },
