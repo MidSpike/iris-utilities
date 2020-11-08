8d12d4fbb9428d543e2357be3ed663d150b23e23
MidSpike <developer@midspike.com>
2020-11-07 19:28:23 -0500

    cleanup

--- src/commands/guild_settings/toggle_disconnect_tts_voice.js
+++ src/commands/guild_settings/toggle_disconnect_tts_voice.js
@@ -2,34 +2,35 @@
 
 //#region local dependencies
 const { CustomRichEmbed } = require('../../libs/CustomRichEmbed.js');
-const { DisBotCommander, DisBotCommand } = require('../../libs/DisBotCommander.js');
+const { DisBotCommand,
+        DisBotCommander } = require('../../libs/DisBotCommander.js');
 //#endregion local dependencies
 
 module.exports = new DisBotCommand({
-    name:'TOGGLE_DISCONNECT_TTS_VOICE',
-    category:`${DisBotCommander.categories.GUILD_SETTINGS}`,
-    weight:13,
-    description:'toggles disconnect tts voice',
-    aliases:['toggle_disconnect_tts_voice'],
-    access_level:DisBotCommand.access_levels.GUILD_ADMIN,
+    name: 'TOGGLE_DISCONNECT_TTS_VOICE',
+    category: `${DisBotCommander.categories.GUILD_SETTINGS}`,
+    weight: 13,
+    description: 'toggles disconnect tts voice',
+    aliases: ['toggle_disconnect_tts_voice'],
+    access_level: DisBotCommand.access_levels.GUILD_ADMIN,
     async executor(Discord, client, message, opts={}) {
         const guild_config = await client.$.guild_configs_manager.fetchConfig(message.guild.id);
         const disconnect_tts_voice = guild_config.disconnect_tts_voice === 'enabled';
         if (disconnect_tts_voice === true) {
             message.channel.send(new CustomRichEmbed({
-                title:`Disconnect TTS Voice: disabled;`,
-                description:`I will no longer use TTS to say something when I leave the voice channel.`
+                title: 'Disconnect TTS Voice: disabled;',
+                description: 'I will no longer use TTS to say something when I leave the voice channel.',
             }, message));
             client.$.guild_configs_manager.updateConfig(message.guild.id, {
-                disconnect_tts_voice:'disabled'
+                disconnect_tts_voice: 'disabled',
             });
         } else {
             message.channel.send(new CustomRichEmbed({
-                title:`Disconnect TTS Voice: enabled;`,
-                description:`I will use TTS to say something when I leave the voice channel.`
+                title: 'Disconnect TTS Voice: enabled;',
+                description: 'I will use TTS to say something when I leave the voice channel.',
             }, message));
             client.$.guild_configs_manager.updateConfig(message.guild.id, {
-                disconnect_tts_voice:'enabled'
+                disconnect_tts_voice: 'enabled',
             });
         }
     },
