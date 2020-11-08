6b6d612cb54b0292529743b660c03b225a9bcb43
MidSpike <developer@midspike.com>
2020-11-08 16:00:09 -0500

    fixed the super people command

--- src/commands/super_people/superpeople.js
+++ src/commands/super_people/superpeople.js
@@ -3,18 +3,19 @@
 //#region local dependencies
 const bot_config = require('../../../config.js');
 
-const { DisBotCommander, DisBotCommand } = require('../../libs/DisBotCommander.js');
+const { DisBotCommand,
+        DisBotCommander } = require('../../libs/DisBotCommander.js');
+const { sendLargeMessage } = require('../../libs/messages.js');
 //#endregion local dependencies
 
 module.exports = new DisBotCommand({
-    name:'SUPERPEOPLE',
-    category:`${DisBotCommander.categories.SUPER_PEOPLE}`,
-    description:'super people',
-    aliases:['superpeople'],
-    access_level:DisBotCommand.access_levels.BOT_SUPER,
+    name: 'SUPERPEOPLE',
+    category: `${DisBotCommander.categories.SUPER_PEOPLE}`,
+    description: 'super people',
+    aliases: ['superpeople'],
+    access_level: DisBotCommand.access_levels.BOT_SUPER,
     async executor(Discord, client, message, opts={}) {
-        message.author.createDM().then(dm_channel => {
-            sendLargeMessage(dm_channel.id, JSON.stringify(bot_config.SUPER_PEOPLE, null, 2));
-        }).catch(console.warn);
+        const dm_channel = await message.author.createDM().catch(console.warn);
+        sendLargeMessage(dm_channel.id, JSON.stringify(bot_config.SUPER_PEOPLE, null, 2))
     },
 });
