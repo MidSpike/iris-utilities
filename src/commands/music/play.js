c8ee4dbe9f2408de9e3bf890676047e0fb127d60
MidSpike <developer@midspike.com>
2020-11-27 12:12:26 -0500

    unlocked `play` command

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -211,10 +211,6 @@ module.exports = new DisBotCommand({
             } else if (detect_broadcastify(command_args.join(' '))) {
                 playBroadcastify(message, command_args.join(' '), playnext);
             } else {
-                if (message.author.id !== '163646957783482370') {
-                    message.reply('YouTube playback functionality is currently locked due to 429 errors from youtube!');
-                    return;
-                }
                 playYouTube(message, command_args.join(' '), playnext);
             }
         } else {
