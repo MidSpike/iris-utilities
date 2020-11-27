d61b3daee00f3c083665e97d171f6bfe1d0c494a
MidSpike <developer@midspike.com>
2020-11-27 12:36:32 -0500

    added cooldown to `skip`

--- src/commands/music/skip.js
+++ src/commands/music/skip.js
@@ -4,22 +4,24 @@
 const bot_config = require('../../../config.js');
 
 const { CustomRichEmbed } = require('../../libs/CustomRichEmbed.js');
-const { DisBotCommander, DisBotCommand } = require('../../libs/DisBotCommander.js');
+const { DisBotCommand,
+        DisBotCommander } = require('../../libs/DisBotCommander.js');
 //#endregion local dependencies
 
 const bot_common_name = bot_config.COMMON_NAME;
 
 module.exports = new DisBotCommand({
-    name:'SKIP',
-    category:`${DisBotCommander.categories.MUSIC}`,
-    weight:8,
-    description:'Allows skipping a song',
-    aliases:['skip', 's', 'next', 'n'],
+    name: 'SKIP',
+    category: `${DisBotCommander.categories.MUSIC}`,
+    weight: 8,
+    description: 'Allows skipping a song',
+    aliases: ['skip', 's', 'next', 'n'],
+    cooldown: 2_500,
     async executor(Discord, client, message, opts={}) {
         const guild_audio_controller = client.$.audio_controllers.get(message.guild.id);
         message.channel.send(new CustomRichEmbed({
-            title:`Controlling ${bot_common_name}`,
-            description:`Skipped the current song!`
+            title: `Controlling ${bot_common_name}`,
+            description: 'Skipped the current song!',
         }, message));
         guild_audio_controller.skip();
     },
