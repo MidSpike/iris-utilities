638cafa2d883ea0b06598add6090f836f7ad2492
MidSpike <developer@midspike.com>
2020-11-27 09:44:40 -0500

    Changed command to new NSFW warning system

--- src/commands/fun/cards.js
+++ src/commands/fun/cards.js
@@ -7,6 +7,7 @@ const { Timer,
         array_make,
         array_random } = require('../../utilities.js');
 
+const { sendPotentiallyNotSafeForWorkDisclaimer } = require('../../libs/messages.js');
 const { CustomRichEmbed } = require('../../libs/CustomRichEmbed.js');
 const { DisBotCommand,
         DisBotCommander } = require('../../libs/DisBotCommander.js');
@@ -18,35 +19,33 @@ module.exports = new DisBotCommand({
     description: 'Cards Against Humanity',
     aliases: ['cards'],
     async executor(Discord, client, message, opts={}) {
-        if (!message.channel.nsfw) {
-            message.channel.send(new CustomRichEmbed({
-                title: 'This command requires an NSFW channel!',
-                description: 'Discord Bot List / Top.gg requires that this command can only be executed in a NSFW channel!',
-            }, message));
-            return;
-        }
-        message.channel.send(new CustomRichEmbed({
+        const potentially_nsfw_content_is_accepted = await sendPotentiallyNotSafeForWorkDisclaimer(message);
+        if (!potentially_nsfw_content_is_accepted) return;
+
+        const bot_message = await message.channel.send(new CustomRichEmbed({
             title: `Cards Against Humanity`,
             description: 'Fetching random card set!\nPlease wait...',
-        })).then(async (bot_message) => {
-            await Timer(1500);
-            const cards_api_res = await axios.get(`https://cards-against-humanity-api.herokuapp.com/sets/Base`);
-            const black_card = array_random(cards_api_res.data.blackCards.filter(card => card.pick === 2));
-            const white_cards = array_make(black_card.pick).map(() => array_random(cards_api_res.data.whiteCards));
-            bot_message.edit(new CustomRichEmbed({
-                title: `Cards Against Humanity`,
-                fields: [
-                    {
-                        name: 'Black Card',
-                        value: `${'```'}\n${black_card.text}\n${'```'}`,
-                    },
-                    ...white_cards.map(card => ({
-                        name: 'White Card',
-                        value: `${'```'}\n${card}\n${'```'}`,
-                        inline: true,
-                    }))
-                ]
-            }, message));
-        });
+        })).catch(console.warn);
+
+        await Timer(1500);
+
+        const cards_api_res = await axios.get(`https://cards-against-humanity-api.herokuapp.com/sets/Base`);
+        const black_card = array_random(cards_api_res.data.blackCards.filter(card => card.pick === 2));
+        const white_cards = array_make(black_card.pick).map(() => array_random(cards_api_res.data.whiteCards));
+
+        bot_message.edit(new CustomRichEmbed({
+            title: `Cards Against Humanity`,
+            fields: [
+                {
+                    name: 'Black Card',
+                    value: `${'```'}\n${black_card.text}\n${'```'}`,
+                },
+                ...white_cards.map(card => ({
+                    name: 'White Card',
+                    value: `${'```'}\n${card}\n${'```'}`,
+                    inline: true,
+                })),
+            ],
+        }, message));
     },
 });
