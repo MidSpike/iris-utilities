304237e94f75a280b3f2cced2f6f4b46e080e08c
MidSpike <developer@midspike.com>
2020-11-27 09:44:54 -0500

    Changed command to new NSFW warning system

--- src/commands/fun/roast.js
+++ src/commands/fun/roast.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //#region local dependencies
-const { CustomRichEmbed } = require('../../libs/CustomRichEmbed.js');
+const { sendPotentiallyNotSafeForWorkDisclaimer } = require('../../libs/messages.js');
 const { DisBotCommander, DisBotCommand } = require('../../libs/DisBotCommander.js');
 
 const { array_random } = require('../../utilities.js');
@@ -10,24 +10,22 @@ const roasts_json = require('../../../files/roasts.json');
 //#endregion local dependencies
 
 module.exports = new DisBotCommand({
-    name:'ROAST',
-    category:`${DisBotCommander.categories.FUN}`,
-    description:'Roast people',
-    aliases:['roast'],
+    name: 'ROAST',
+    category: `${DisBotCommander.categories.FUN}`,
+    description: 'Roast people',
+    aliases: ['roast'],
     async executor(Discord, client, message, opts={}) {
-        if (!message.channel.nsfw) {
-            message.channel.send(new CustomRichEmbed({
-                title:'This command requires an NSFW channel!',
-                description:'Discord Bot List / Top.gg requires that this command can only be executed in a NSFW channel!'
-            }, message));
-            return;
-        }
+        const potentially_nsfw_content_is_accepted = await sendPotentiallyNotSafeForWorkDisclaimer(message);
+        if (!potentially_nsfw_content_is_accepted) return;
+
         const roaster = message.author;
         const roastee = message.mentions.users.first() ?? message.author;
         const roast = `${array_random(roasts_json)}`;
-        message.channel.send(`${roaster} is roasting ${roastee}\nHey ${roastee} ${roast}`);
+
+        await message.channel.send(`${roaster} is roasting ${roastee}\nHey ${roastee} ${roast}`).catch(console.warn);
+
         if (roaster?.id === roastee?.id) {
-            message.channel.send(`Next time you should try roasting someone besides yourself!`);
+            message.channel.send(`Next time you should try roasting someone besides yourself!`).catch(console.warn);
         }
     },
 });
