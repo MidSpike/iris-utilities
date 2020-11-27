2803567421a43fda7a78440193f2948d085a1922
MidSpike <developer@midspike.com>
2020-11-27 12:36:47 -0500

    added cooldown to `replay`

--- src/commands/music/replay.js
+++ src/commands/music/replay.js
@@ -3,24 +3,26 @@
 //#region local dependencies
 const { createConnection } = require('../../libs/createConnection.js');
 const { CustomRichEmbed } = require('../../libs/CustomRichEmbed.js');
-const { DisBotCommander, DisBotCommand } = require('../../libs/DisBotCommander.js');
+const { DisBotCommand,
+        DisBotCommander } = require('../../libs/DisBotCommander.js');
 //#endregion local dependencies
 
 module.exports = new DisBotCommand({
-    name:'REPLAY',
-    category:`${DisBotCommander.categories.MUSIC}`,
-    weight:9,
-    description:'Allows replaying a song',
-    aliases:['replay', 'r'],
+    name: 'REPLAY',
+    category: `${DisBotCommander.categories.MUSIC}`,
+    weight: 9,
+    description: 'Allows replaying a song',
+    aliases: ['replay', 'r'],
+    cooldown: 10_000,
     async executor(Discord, client, message, opts={}) {
         const guild_queue_manager = client.$.queue_managers.get(message.guild.id);
 
         const voice_channel = message.member.voice.channel;
         if (!voice_channel) {
             message.channel.send(new CustomRichEmbed({
-                color:0xFFFF00,
-                title:`Well that's an issue!`,
-                description:`You need to be in a voice channel to use this command!`
+                color: 0xFFFF00,
+                title: `Well that's an issue!`,
+                description: `You need to be in a voice channel to use this command!`,
             }, message));
             return;
         }
@@ -31,9 +33,9 @@ module.exports = new DisBotCommand({
             guild_queue_manager.addItem(thing_to_replay, 1);
         } else {
             message.channel.send(new CustomRichEmbed({
-                color:0xFFFF00,
-                title:'Sorry, I forgot something along the way!',
-                description:`It would seem that I'm unable to replay that!`
+                color: 0xFFFF00,
+                title: 'Sorry, I forgot something along the way!',
+                description: `It would seem that I'm unable to replay that!`,
             }, message));
         }
     },
