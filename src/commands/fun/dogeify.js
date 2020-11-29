998df7cd31934ae0d3d434234fcbb005d3192b78
MidSpike <developer@midspike.com>
2020-11-29 14:14:38 -0500

    cleanup

--- src/commands/fun/dogeify.js
+++ src/commands/fun/dogeify.js
@@ -3,43 +3,48 @@
 //#region local dependencies
 const dogeify = require('dogeify-js');
 
+const { Timer } = require('../../utilities.js');
+
 const { CustomRichEmbed } = require('../../libs/CustomRichEmbed.js');
-const { DisBotCommander, DisBotCommand } = require('../../libs/DisBotCommander.js');
+const { DisBotCommand,
+        DisBotCommander } = require('../../libs/DisBotCommander.js');
 //#endregion local dependencies
 
 const bot_cdn_url = process.env.BOT_CDN_URL;
 
 module.exports = new DisBotCommand({
-    name:'DOGEIFY',
-    category:`${DisBotCommander.categories.FUN}`,
-    description:'Dogeify text',
-    aliases:['dogeify'],
+    name: 'DOGEIFY',
+    category: `${DisBotCommander.categories.FUN}`,
+    description: 'Translate sentences into Doge-speak -> so sentences. amaze.',
+    aliases: ['dogeify'],
     async executor(Discord, client, message, opts={}) {
         const { clean_command_args } = opts;
+
         const user_text = clean_command_args.join(' ');
+
         if (user_text.length === 0) {
             message.channel.send(new CustomRichEmbed({
-                color:0xFFFF00,
-                title:`Couldn't dogeify:`,
-                description:`Try typing a sentence after the command!`,
-                thumbnail:`${bot_cdn_url}/doge-static.gif`
+                color: 0xFFFF00,
+                title: 'Couldn\'t dogeify:',
+                description: 'Try typing a sentence after the command!',
+                thumbnail: `${bot_cdn_url}/doge-static.gif`,
             }, message));
         } else {
-            message.channel.send(new CustomRichEmbed({
-                title:'Dogeifying:',
-                description:`\`\`\`${user_text}\`\`\``,
-                image:`${bot_cdn_url}/doge-animated.gif`
-            }, message)).then(bot_message => {
-                dogeify(user_text).then(dogeified_text => {
-                    client.setTimeout(() => {
-                        bot_message.edit(new CustomRichEmbed({
-                            title:'Dogeified',
-                            description:`\`\`\`${user_text}\`\`\`into\`\`\`${dogeified_text}\`\`\``,
-                            thumbnail:`${bot_cdn_url}/doge-static.jpg`
-                        }, message));
-                    }, 3500);
-                });
-            });
+            const bot_message = await message.channel.send(new CustomRichEmbed({
+                title: 'Dogeifying:',
+                description: `${'```'}\n${user_text}\n${'```'}`,
+                image: `${bot_cdn_url}/doge-animated.gif`,
+            }, message)).catch(console.warn);
+
+            const dogeified_text = await dogeify(user_text);
+
+            await Timer(3500);
+
+            bot_message.edit(new CustomRichEmbed({
+                title: 'Dogeified',
+                description: `${'```'}\n${user_text}\n${'```'}into${'```'}\n${dogeified_text}\n${'```'}`,
+                thumbnail: `${bot_cdn_url}/doge-static.jpg`,
+            }, message)).catch(console.warn);
         }
     },
 });
