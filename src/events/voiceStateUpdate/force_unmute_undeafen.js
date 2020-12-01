d3b143e1e693284f189a3339809e0964ae9b5662
MidSpike <developer@midspike.com>
2020-11-30 20:56:50 -0500

    cleanup

--- src/events/voiceStateUpdate/force_unmute_undeafen.js
+++ src/events/voiceStateUpdate/force_unmute_undeafen.js
@@ -14,11 +14,11 @@ module.exports = {
         if (client.$.restarting_bot) return;
 
         if (isThisBot(new_voice_state.member.id)) {
-            if (new_voice_state.connection && new_voice_state.channel) { // Run if connected to a voice channel
-                await Timer(500); // prevent API abuse
-                if (new_voice_state.serverMute) new_voice_state.setMute(false, `Don't mute me!`);
-                if (new_voice_state.serverDeaf) new_voice_state.setDeaf(false, `Don't deafen me!`);
+            if (new_voice_state.connection && new_voice_state.channel) { // run if connected to a voice channel
+                await Timer(750); // prevent API abuse
+                if (new_voice_state.serverMute) new_voice_state.setMute(false, 'Don\'t mute me!');
+                if (new_voice_state.serverDeaf) new_voice_state.setDeaf(false, 'Don\'t deafen me!');
             }
         }
-    }
+    },
 };
