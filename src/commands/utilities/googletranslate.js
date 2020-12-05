320789300312ac92cb9beab610e8c83f04e2fbac
MidSpike <developer@midspike.com>
2020-12-04 19:43:14 -0500

    cleanup

--- src/commands/utilities/googletranslate.js
+++ src/commands/utilities/googletranslate.js
@@ -6,18 +6,18 @@ const axios = require('axios');
 const { Timer } = require('../../utilities.js');
 
 const { CustomRichEmbed } = require('../../libs/CustomRichEmbed.js');
-const { DisBotCommander,
-        DisBotCommand } = require('../../libs/DisBotCommander.js');
+const { DisBotCommand,
+        DisBotCommander} = require('../../libs/DisBotCommander.js');
 //#endregion local dependencies
 
 const bot_api_url = process.env.BOT_API_SERVER_URL;
 
 module.exports = new DisBotCommand({
-    name:'GOOGLETRANSLATE',
-    category:`${DisBotCommander.categories.UTILITIES}`,
-    weight:13,
-    description:'Google Translate',
-    aliases:['googletranslate'],
+    name: 'GOOGLETRANSLATE',
+    category: `${DisBotCommander.categories.UTILITIES}`,
+    weight: 13,
+    description: 'Google Translate',
+    aliases: ['googletranslate', 'translate'],
     async executor(Discord, client, message, opts={}) {
         const { discord_command, clean_command_args } = opts;
 
@@ -25,12 +25,15 @@ module.exports = new DisBotCommand({
 
         if (user_text.length === 0) {
             message.channel.send(new CustomRichEmbed({
-                color:0xFFFF00,
-                title:`Couldn't Translate:`,
-                description:`Try typing a sentence after the command!`,
-                fields:[
-                    {name:'Example Usage', value:`${discord_command} hallo dort aus Deutschland!`}
-                ]
+                color: 0xFFFF00,
+                title: 'I couldn\'t translate that:',
+                description: 'Try typing a sentence after the command!',
+                fields: [
+                    {
+                        name: 'Example Usage',
+                        value: `${discord_command} hallo dort aus Deutschland!`
+                    },
+                ],
             }, message));
         } else {
             const lang_code_match = user_text.match(/\{(.*)\}/);
@@ -38,8 +41,8 @@ module.exports = new DisBotCommand({
             const translate_this = user_text.replace(`{${translate_to}}`, '').trim();
 
             const bot_message = await message.channel.send(new CustomRichEmbed({
-                title:'Translating...',
-                description:`${'```'}\n${translate_this}\n${'```'}`
+                title: 'Translating...',
+                description: `${'```'}\n${translate_this}\n${'```'}`,
             }, message));
 
             const api_response = await axios.get(`${bot_api_url}/translate?token=${encodeURIComponent(process.env.BOT_API_SERVER_TOKEN)}&translate_to=${encodeURIComponent(translate_to)}&text=${encodeURIComponent(user_text)}`);
@@ -49,14 +52,19 @@ module.exports = new DisBotCommand({
                     translated_to_language,
                     translated_text } = api_response.data;
 
-            await Timer(1500);
+            await Timer(1500); // prevent api abuse
 
             bot_message.edit(new CustomRichEmbed({
-                title:'Translated!',
-                fields:[
-                    {name:`From {${translated_from_language}}`, value:`${'```'}\n${original_text}\n${'```'}`},
-                    {name:`To {${translated_to_language}}`, value:`${'```'}\n${translated_text}\n${'```'}`}
-                ]
+                title: 'Translated!',
+                fields: [
+                    {
+                        name: `From {${translated_from_language}}`,
+                        value: `${'```'}\n${original_text}\n${'```'}`
+                    }, {
+                        name: `To {${translated_to_language}}`,
+                        value: `${'```'}\n${translated_text}\n${'```'}`
+                    },
+                ],
             }, message));
         }
     },
