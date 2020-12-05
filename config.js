9deb74609de8cbf0c97d2531ae9e78647019bff0
MidSpike <developer@midspike.com>
2020-12-04 21:15:32 -0500

    Bumped `PUBLIC_VERSION`

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ const { Collection } = require('discord.js');
  * semi-version:
  * - used to denote minor patches to an already released PUBLIC_VERSION
  */
-const PUBLIC_VERSION = 'vS_2020-12-04_0';
+const PUBLIC_VERSION = 'vS_2020-12-04_1';
 
 /**
  * The various names given to this bot matching the schemes below
