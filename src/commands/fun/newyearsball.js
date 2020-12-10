03e59cf3c5a9a9872f515f160149f62dbbd10ae5
MidSpike <developer@midspike.com>
2020-12-10 17:21:34 -0500

    updated `newyearsball` for 2020 -> 2021

--- src/commands/fun/newyearsball.js
+++ src/commands/fun/newyearsball.js
@@ -7,24 +7,25 @@ const path = require('path');
 const { Timer } = require('../../utilities.js');
 
 const { CustomRichEmbed } = require('../../libs/CustomRichEmbed.js');
-const { DisBotCommander, DisBotCommand } = require('../../libs/DisBotCommander.js');
+const { DisBotCommand,
+        DisBotCommander } = require('../../libs/DisBotCommander.js');
 //#endregion local dependencies
 
 module.exports = new DisBotCommand({
-    name:'NEWYEARSBALL',
-    category:`${DisBotCommander.categories.FUN}`,
-    description:'newyearsball',
-    aliases:['newyearsball'],
+    name: 'NEWYEARSBALL',
+    category: `${DisBotCommander.categories.FUN}`,
+    description: 'newyearsball',
+    aliases: ['newyearsball'],
     async executor(Discord, client, message, opts={}) {
         const ball_message = await message.channel.send(new CustomRichEmbed({
-            title:`Preparing New Years Ball`
+            title: 'Preparing New Years Ball',
         }, message));
         for (let ball_number = 1; ball_number <= 11; ball_number++) {
             const ball_path = path.join(process.cwd(), `./files/new_years_ball/ball_${ball_number}.txt`);
             const ball_art = fs.readFileSync(ball_path).toString();
             await ball_message.edit(new CustomRichEmbed({
-                title:`Ball ${ball_number}`,
-                description:`${'```'}\n${ball_art}\n${'```'}`
+                title: `Ball ${ball_number}`,
+                description: `${'```'}\n${ball_art}\n${'```'}`,
             }));
             await Timer(1000);
         }
