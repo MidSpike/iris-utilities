a1699bbb7c0b3ba0c067a40163e2b5e25d8d1409
MidSpike <developer@midspike.com>
2020-12-10 17:24:51 -0500

    cleanup

--- src/commands/super_people/reload_all.js
+++ src/commands/super_people/reload_all.js
@@ -1,38 +1,42 @@
 'use strict';
 
 //#region local dependencies
-const fs = require('fs');
 const path = require('path');
 const recursiveReadDirectory = require('recursive-read-directory');
 
-const { DisBotCommander, DisBotCommand } = require('../../libs/DisBotCommander.js');
+const { DisBotCommand,
+        DisBotCommander } = require('../../libs/DisBotCommander.js');
 
 const { sendNotAllowedCommand } = require('../../libs/messages.js');
-const { isSuperPerson, isSuperPersonAllowed } = require('../../libs/permissions.js');
+const { isSuperPerson,
+        isSuperPersonAllowed } = require('../../libs/permissions.js');
 //#endregion local dependencies
 
 module.exports = new DisBotCommand({
-    name:'RELOAD_ALL',
-    category:`${DisBotCommander.categories.SUPER_PEOPLE}`,
-    description:'reloads all commands',
-    aliases:['reload_all'],
-    access_level:DisBotCommand.access_levels.BOT_SUPER,
+    name: 'RELOAD_ALL',
+    category: `${DisBotCommander.categories.SUPER_PEOPLE}`,
+    description: 'reloads all commands',
+    aliases: ['reload_all'],
+    access_level: DisBotCommand.access_levels.BOT_SUPER,
     async executor(Discord, client, message, opts={}) {
         if (!isSuperPersonAllowed(isSuperPerson(message.member.id), 'reload')) {
             sendNotAllowedCommand(message);
             return;
         }
-        await message.reply(`Reloading All Command Files`);
+
+        await message.reply('Reloading All Command Files').catch(console.warn);
+
         try {
             for (const command_to_reload of DisBotCommander.commands.values()) {
                 const command_files_directory_path = path.join(process.cwd(), './src/commands/');
                 const command_files = recursiveReadDirectory(command_files_directory_path).filter(file => file.endsWith('.js'));
-    
+
                 const command_file_to_reload = command_files.find(cmd_file_path => path.parse(cmd_file_path).base === `${command_to_reload.name.toLowerCase()}.js`);
-    
+
                 const command_file_path_to_reload = path.join(process.cwd(), `./src/commands/`, command_file_to_reload);
-    
+
                 delete require.cache[require.resolve(command_file_path_to_reload)]; // IMPORTANT: Uncache the module first!
+
                 const reloaded_command = require(command_file_path_to_reload);
                 if (reloaded_command) {
                     DisBotCommander.registerCommand(reloaded_command);
@@ -41,8 +45,9 @@ module.exports = new DisBotCommand({
                     throw new Error(`Unable to Reload Command File: ${command_file_to_reload}`);
                 }
             }
-            message.reply(`Finished Reloading All Command Files`);
+            message.reply('Finished Reloading All Command Files');
         } catch (error) {
+            console.trace(error);
             message.reply(`${error}`);
         }
     },
