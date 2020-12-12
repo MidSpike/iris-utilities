c35451a792ae2303df8caa2242435c54112a77a4
MidSpike <developer@midspike.com>
2020-12-12 17:03:29 -0500

    worked on history requests system

--- src/commands/help_info/request_history_deletion.js
+++ src/commands/help_info/request_history_deletion.js
@@ -1,24 +1,37 @@
 'use strict';
 
 //#region local dependencies
+const moment = require('moment-timezone');
+
 const { CustomRichEmbed } = require('../../libs/CustomRichEmbed.js');
-const { DisBotCommander, DisBotCommand } = require('../../libs/DisBotCommander.js');
+const { DisBotCommand,
+        DisBotCommander } = require('../../libs/DisBotCommander.js');
+const { sendConfirmationMessage } = require('../../libs/messages.js');
 //#endregion local dependencies
 
 const history_deletion_requests_channel_id = process.env.BOT_LOGGING_CHANNEL_HISTORY_DELETION_REQUESTS_ID;
 
 module.exports = new DisBotCommand({
-    name:'REQUEST_HISTORY_DELETION',
-    category:`${DisBotCommander.categories.HELP_INFO}`,
-    weight:17,
-    description:'Allows the user to request their history to be deleted',
-    aliases:['request_history_deletion'],
+    name: 'REQUEST_HISTORY_DELETION',
+    category: `${DisBotCommander.categories.HELP_INFO}`,
+    weight: 17,
+    description: 'Allows the user to request their history to be deleted',
+    aliases: ['request_history_deletion'],
     async executor(Discord, client, message, opts={}) {
-        const history_deletion_requests_channel = client.channels.cache.get(history_deletion_requests_channel_id);
-        await history_deletion_requests_channel.send(`@${message.author.tag} (${message.author.id}) Requested to have their history deleted!`).catch(console.trace);
-        message.reply(new CustomRichEmbed({
-            title: 'Success! Your command history will be deleted within 24 hours!',
-            description: `Keep in mind that essential data (such as ban records) will not be deleted!`,
-        }, message)).catch(console.warn);
+        const confirmation_embed = new CustomRichEmbed({
+            color: 0xFF00FF,
+            description: `Do you want to request for your stored user history to be deleted?`,
+        }, message);
+        const yes_callback = async () => {
+            const history_deletion_requests_channel = client.channels.cache.get(history_deletion_requests_channel_id);
+            await history_deletion_requests_channel.send(`@${message.author.tag} (${message.author.id}) - ${moment()}`).catch(console.trace);
+            message.reply(new CustomRichEmbed({
+                color: 0xFF00FF,
+                title: 'Your command history will be removed within 48 hours!',
+                description: 'Keep in mind that essential data (such as ban records) will not be deleted!',
+            }, message)).catch(console.warn);
+        };
+        const no_callback = () => {};
+        sendConfirmationMessage(message.author.id, message.channel.id, true, confirmation_embed, yes_callback, no_callback);
     },
 });
