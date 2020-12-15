292e88df08ed63a71458c9fa6185c99fcbb18d85
MidSpike <developer@midspike.com>
2020-12-15 18:44:04 -0500

    added cooldown

--- src/commands/help_info/request_history_deletion.js
+++ src/commands/help_info/request_history_deletion.js
@@ -17,17 +17,18 @@ module.exports = new DisBotCommand({
     weight: 17,
     description: 'Allows the user to request their history to be deleted',
     aliases: ['request_history_deletion'],
+    cooldown: 60_000,
     async executor(Discord, client, message, opts={}) {
         const confirmation_embed = new CustomRichEmbed({
             color: 0xFF00FF,
-            description: `Do you want to request for your stored user history to be deleted?`,
+            description: 'Do you want to request for your stored user history to be deleted?',
         }, message);
         const yes_callback = async () => {
             const history_deletion_requests_channel = client.channels.cache.get(history_deletion_requests_channel_id);
             await history_deletion_requests_channel.send(`@${message.author.tag} (${message.author.id}) - ${moment()}`).catch(console.trace);
             message.reply(new CustomRichEmbed({
                 color: 0xFF00FF,
-                title: 'Your command history will be removed within 48 hours!',
+                title: 'Your user history will be removed within 48 hours!',
                 description: 'Keep in mind that essential data (such as ban records) will not be deleted!',
             }, message)).catch(console.warn);
         };
