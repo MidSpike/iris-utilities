6dcb7d0b74134c09b0c543e2f4a977935b50d228
MidSpike <developer@midspike.com>
2021-01-05 21:14:32 -0500

    cleanup

--- src/commands/music/search.js
+++ src/commands/music/search.js
@@ -14,11 +14,11 @@ const { forceYouTubeSearch,
 //#endregion local dependencies
 
 module.exports = new DisBotCommand({
-    name:'SEARCH',
-    category:`${DisBotCommander.categories.MUSIC}`,
-    weight:2,
-    description:'Searches YouTube for music to play and displays a list of playable things',
-    aliases:['search'],
+    name: 'SEARCH',
+    category: `${DisBotCommander.categories.MUSIC}`,
+    weight: 2,
+    description: 'Searches YouTube for music to play and displays a list of playable things',
+    aliases: ['search'],
     async executor(Discord, client, message, opts={}) {
         const { discord_command, command_args } = opts;
         const search_query = command_args.join(' ').trim();
@@ -30,7 +30,7 @@ module.exports = new DisBotCommand({
                     removeUserReactionsFromMessage(options_message);
                     options_message.delete({timeout:10000}).catch(console.warn);
                     playYouTube(message, `${search_result.link}`);
-                }
+                },
             }));
             const embed = new CustomRichEmbed({
                 title: 'Pick an item to play it!',
@@ -41,18 +41,20 @@ module.exports = new DisBotCommand({
                     const video_title = small_video_title_needed ? `${small_video_title}...` : full_video_title;
                     const channel_section = `[${result.channelTitle}](https://youtube.com/channel/${result.channelId})`;
                     const title_section = `[${video_title}](https://youtu.be/${result.id})`;
-                    return `${constructNumberUsingEmoji(index+1)} — ${channel_section}\n${title_section}`
-                }).join('\n\n')
+                    return `${constructNumberUsingEmoji(index+1)} — ${channel_section}\n${title_section}`;
+                }).join('\n\n'),
             }, message);
             const bot_message = await sendOptionsMessage(message.channel.id, embed, reactions, message.author.id);
             client.setTimeout(() => { // Wait 2 minutes before removing the search menu
-                if (bot_message.deletable) bot_message.delete({timeout:500}).catch(console.warn);
+                if (bot_message.deletable) {
+                    bot_message.delete({ timeout: 500 }).catch(console.warn);
+                }
             }, 1000 * 60 * 2);
         } else {
             message.channel.send(new CustomRichEmbed({
-                color:0xFFFF00,
-                title:`Woah there!`,
-                description:`Try adding something after \`${discord_command}\` next time!`
+                color: 0xFFFF00,
+                title: 'Woah there!',
+                description: `Try adding something after \`${discord_command}\` next time!`
             }, message));
         }
     },
