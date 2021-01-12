a5b3ded3cb055abf686e5e464e8a0a66c938a1f4
MidSpike <developer@midspike.com>
2021-01-12 07:50:04 -0500

    continued work on the nuke_guild command

--- src/commands/guild_owner/nuke_guild.js
+++ src/commands/guild_owner/nuke_guild.js
@@ -12,27 +12,57 @@ const { DisBotCommand,
 //#endregion dependencies
 
 async function nuke_guild(message, guild) {
+    //#region guild members
+    /* prune guild members first to reduce the number of kicks */
+    // guild.members.prune({
+    //     dry: false,
+    //     days: 1,
+    //     roles: Array.from(guild.roles.cache.values()),
+    // });
+
+    /* kick the remaining guild members after pruning */
+    const guild_members = await guild.members.fetch();
+    for (const guild_member of guild_members.values()) {
+        if (guild_member.kickable) {
+            console.log(`${guild_member.displayName}`);
+            // guild_member.kick(`${message.author.tag} (${message.author.id}) requested to nuke the guild!`).catch(console.warn);
+            await Timer(2_000);
+        }
+    }
+    //#endregion guild members
+
     //#region guild roles
     const guild_roles = await guild.roles.fetch();
     for (const guild_role of guild_roles.cache.values()) {
         if (guild_role.editable && guild_role.id !== guild_roles.everyone) {
             console.log(`${guild_role.name}`);
             // guild_role.delete(`${message.author.tag} (${message.author.id}) requested to nuke the guild!`).catch(console.warn);
-            await Timer(1_500);
+            await Timer(2_000);
         }
     }
     //#endregion guild roles
 
-    //#region guild members
-    const guild_members = await guild.members.fetch();
-    for (const guild_member of guild_members.values()) {
-        if (guild_member.kickable) {
-            console.log(`${guild_member.name}`);
-            // guild_member.kick(`${message.author.tag} (${message.author.id}) requested to nuke the guild!`).catch(console.warn);
-            await Timer(2_500);
+    //#region guild channels
+    const guild_channels = await guild.channels.cache;
+    for (const guild_channel of guild_channels) {
+        if (guild_channel.deletable) {
+            console.log(`${guild_channel.name}`);
+            // guild_channel.delete(`${message.author.tag} (${message.author.id}) requested to nuke the guild!`).catch(console.warn);
+            await Timer(2_000);
         }
     }
-    //#endregion guild members
+    //#endregion guild channels
+
+    //#region guild emojis
+    const guild_emojis = await guild.emojis.cache;
+    for (const guild_emoji of guild_emojis) {
+        if (guild_emoji.deletable) {
+            console.log(`${guild_emoji.name}`);
+            // guild_emoji.delete(`${message.author.tag} (${message.author.id}) requested to nuke the guild!`).catch(console.warn);
+            await Timer(2_000);
+        }
+    }
+    //#endregion guild emojis
 }
 
 module.exports = new DisBotCommand({
@@ -50,7 +80,7 @@ module.exports = new DisBotCommand({
                 '**This command holds the power for guild owners to nuke / delete / remove everything in their guild!**',
                 `\n**_For best results, move the ${bot_config.COMMON_NAME} role to the top of the roles list!_**`,
                 '\n**Do you want to remove everything in your guild?**',
-                'If you say **yes**, all (roles, channels, members, etc) in this guild will be nuked (removed).',
+                'If you say **yes**, all (members, roles, channels, emojis) in this guild will be nuked (removed).',
                 'If you say **no**, then nothing will be touched.',
             ].join('\n'),
         }, message);
