db08711e98a1ca2174179754540a7c79b0aaa4bd
MidSpike <developer@midspike.com>
2021-01-17 17:07:05 -0500

    cleanup

--- src/commands/music/skip.js
+++ src/commands/music/skip.js
@@ -1,14 +1,10 @@
 'use strict';
 
 //#region local dependencies
-const bot_config = require('../../../config.js');
-
 const { CustomRichEmbed } = require('../../libs/CustomRichEmbed.js');
 const { DisBotCommand,
         DisBotCommander } = require('../../libs/DisBotCommander.js');
 //#endregion local dependencies
-
-const bot_common_name = bot_config.COMMON_NAME;
 
 module.exports = new DisBotCommand({
     name: 'SKIP',
@@ -20,8 +16,7 @@ module.exports = new DisBotCommand({
     async executor(Discord, client, message, opts={}) {
         const guild_audio_controller = client.$.audio_controllers.get(message.guild.id);
         message.channel.send(new CustomRichEmbed({
-            title: `Controlling ${bot_common_name}`,
-            description: 'Skipped the current song!',
+            title: 'Skipped the current song!',
         }, message));
         guild_audio_controller.skip();
     },
