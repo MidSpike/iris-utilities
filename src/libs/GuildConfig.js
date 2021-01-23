cf09b83947b76c1d46297db882a8ba02ba1037d2
MidSpike <developer@midspike.com>
2021-01-22 20:46:48 -0500

    exported GuildConfigsManager to DataConfigsManager

--- src/libs/GuildConfig.js
+++ src/libs/GuildConfig.js
@@ -1,107 +1,13 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
-const { Collection } = require('discord.js');
-
-const { object_sort } = require('../utilities.js');
-
-//---------------------------------------------------------------------------------------------------------------//
-
-/**
- * @typedef {String} GuildId 
- * @typedef {Object} GuildConfig 
- * @typedef {Collection<GuildId, GuildConfig>} GuildConfigs 
- */
+const { DataConfigsManager } = require('./base/DataConfigsManager.js');
 
 //---------------------------------------------------------------------------------------------------------------//
 
 /**
  * Creates an interface for interacting with Guild Configs
- * @param {String} configs_file_relative_path a relative file path to the `.json` file from `process.cwd()`
  */
-class GuildConfigsManager {
-    /** @type {String} */
-    #configs_file;
-
-    /** @type {GuildConfigs} */
-    #configs_in_memory;
-
-    constructor(configs_file_relative_path) {
-        if (typeof configs_file_relative_path !== 'string') throw new TypeError('\`configs_file_relative_path\` must be a string!');
-
-        /* set `this.#configs_file` to the absolute file path */
-        this.#configs_file = path.join(process.cwd(), configs_file_relative_path);
-
-        /* retrieve configs from storage as: Array<Array<guild_id='', guild_config_data={}>> */
-        const configs_from_storage = JSON.parse(fs.readFileSync(this.#configs_file));
-
-        /* prepare the configs in memory to be stored as a `Discord.Collection` */
-        this.#configs_in_memory = new Collection(configs_from_storage);
-
-        /* automatically save the configs from memory to storage every 30 minutes */
-        setInterval(() => {
-            this.saveConfigs();
-        }, 1000 * 60 * 30);
-    }
-
-    /**
-     * Retrieves all guild configs
-     * @returns {GuildConfigs} all guild configs
-     */
-    get configs() {
-        return this.#configs_in_memory;
-    }
-
-    /**
-     * Retrieves the config of the specified guild
-     * @param {GuildId} guild_id 
-     * @returns {GuildConfig} a key:value guild config in the form of an object literal
-     */
-    async fetchConfig(guild_id) {
-        return this.#configs_in_memory.get(guild_id) ?? {};
-    }
-
-    /**
-     * Updates the config of the specified guild
-     * @param {GuildId} guild_id 
-     * @param {GuildConfig} partial_config_data a key:value partial guild config in the form of an object literal
-     * @param {Boolean} keep_old_config_data keep existing config entries or start from scratch
-     * @returns {GuildConfigsManager} this GuildConfigsManager
-     */
-    async updateConfig(guild_id, partial_config_data={}, keep_old_config_data=true) {
-        const old_config_data = await this.fetchConfig(guild_id);
-
-        const new_config_data = {
-            ...(keep_old_config_data ? old_config_data : {}),
-            ...partial_config_data,
-        };
-
-        this.#configs_in_memory.set(guild_id, object_sort(new_config_data));
-        return this;
-    }
-
-    /**
-     * Removes the config of the specified guild
-     * @param {GuildId} guild_id 
-     * @returns {GuildConfigsManager} this GuildConfigsManager
-     */
-    async removeConfig(guild_id) {
-        this.#configs_in_memory.delete(guild_id);
-        return this;
-    }
-
-    /**
-     * Writes all of the guild configs to the guild configs file
-     * @returns {GuildConfigsManager} this GuildConfigsManager
-     */
-    async saveConfigs() {
-        console.time('Saving guild configs to storage!');
-        fs.writeFileSync(this.#configs_file, JSON.stringify(Array.from(this.#configs_in_memory), null, 2));
-        console.timeEnd('Saving guild configs to storage!');
-        return this;
-    }
-}
+class GuildConfigsManager extends DataConfigsManager {}
 
 module.exports = {
     GuildConfigsManager,
