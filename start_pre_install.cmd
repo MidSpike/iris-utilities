22e1c31d4448549cca2c42bdfbedb1c6c13a8fdc
MidSpike <developer@midspike.com>
2021-02-05 10:02:34 -0500

    automatically creates certain directories

--- start_pre_install.cmd
+++ start_pre_install.cmd
@@ -2,6 +2,13 @@
 
 title I.R.I.S. Utilities Pre-Install
 
+echo installing required dependencies
 npm i
+
+echo creating required directories
+if not exist ".\logging\commands" mkdir ".\logging\commands"
+if not exist ".\logging\updates" mkdir ".\logging\updates"
+if not exist ".\database" mkdir ".\database"
+if not exist ".\temporary" mkdir ".\temporary"
 
 pause
