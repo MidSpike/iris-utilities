50412510db36c369d47393e311277063bd9464f9
MidSpike <developer@midspike.com>
2021-02-06 11:51:08 -0500

    fixes issue with `npm i` exiting script early

--- start_pre_install.cmd
+++ start_pre_install.cmd
@@ -2,13 +2,14 @@
 
 title I.R.I.S. Utilities Pre-Install
 
-echo installing required dependencies
-npm i
+echo Installing required dependencies
+call npm i
 
-echo creating required directories
+echo Creating required directories
 if not exist ".\logging\commands" mkdir ".\logging\commands"
 if not exist ".\logging\updates" mkdir ".\logging\updates"
 if not exist ".\database" mkdir ".\database"
 if not exist ".\temporary" mkdir ".\temporary"
 
-pause
+echo Starting bot in 3 seconds
+timeout /T 3 /NOBREAK
