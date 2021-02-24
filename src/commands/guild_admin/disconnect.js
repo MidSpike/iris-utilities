2e8c63f4a0817e2f4dbc4184fde77fa88dd49dc2
MidSpike <developer@midspike.com>
2021-02-23 21:27:31 -0500

    cleanup and fixes for disconnect

--- src/commands/guild_admin/disconnect.js
+++ src/commands/guild_admin/disconnect.js
@@ -1,40 +1,70 @@
 'use strict';
 
 //#region dependencies
+const { DisBotCommand,
+        DisBotCommander } = require('../../libs/DisBotCommander.js');
 const { CustomRichEmbed } = require('../../libs/CustomRichEmbed.js');
-const { DisBotCommand, DisBotCommander } = require('../../libs/DisBotCommander.js');
+const { isThisBot,
+        isThisBotsOwner,
+        botHasPermissionsInGuild } = require('../../libs/permissions.js');
 const { logAdminCommandsToGuild } = require('../../libs/messages.js');
-const { botHasPermissionsInGuild, isThisBot, isThisBotsOwner } = require('../../libs/permissions.js');
 //#endregion dependencies
 
 module.exports = new DisBotCommand({
-    name:'DISCONNECT',
-    category:`${DisBotCommander.categories.GUILD_ADMIN}`,
-    description:'Disconnects a user from their voice channel',
-    aliases:['disconnect'],
-    access_level:DisBotCommand.access_levels.GUILD_MOD,
+    name: 'DISCONNECT',
+    category: `${DisBotCommander.categories.GUILD_ADMIN}`,
+    description: 'Disconnects a user from their voice channel',
+    aliases: ['disconnect'],
+    access_level: DisBotCommand.access_levels.GUILD_MOD,
     async executor(Discord, client, message, opts={}) {
         const { discord_command, command_args } = opts;
+
         if (!botHasPermissionsInGuild(message, ['MOVE_MEMBERS'])) return;
-        const user = client.users.resolve(command_args[0]) ?? message.mentions.users.first();
-        if (!user) {
+
+        const member = message.guild.members.resolve(command_args[0]) ?? message.mentions.members.first();
+        if (!member) {
             message.channel.send(new CustomRichEmbed({
-                color:0xFFFF00,
-                title:'Provide a @user next time!'
+                color: 0xFFFF00,
+                description: 'Provide a user @mention next time!',
             }, message));
             return;
         }
-        message.guild.members.fetch(user.id).then(guildMember => {
-            if (isThisBotsOwner(guildMember.id) || isThisBot(guildMember.id) || guildMember.id === message.author.id) return;
-            if (!guildMember.voice) return;
-            guildMember.voice.kick(`@${message.author.username} used ${discord_command}`).then(() => {
+
+        if (isThisBotsOwner(member.id) || isThisBot(member.id) || member.id === message.author.id) {
+            message.channel.send(new CustomRichEmbed({
+                color: 0xFFFF00,
+                description: 'You aren\'t allowed to disconnect this user!',
+            }, message));
+            return;
+        }
+
+        const member_voice = member.voice;
+        const member_voice_channel = member_voice.channel;
+        if (!member_voice_channel) {
+            message.channel.send(new CustomRichEmbed({
+                color: 0xFFFF00,
+                description: 'That user is not in a voice channel!',
+            }, message));
+            return;
+        }
+
+        let disconnected_voice_member;
+        try {
+            disconnected_voice_member = await member_voice.kick(`@${message.author.username} used ${discord_command}`);
+        } catch {
+            message.channel.send(new CustomRichEmbed({
+                color: 0xFF0000,
+                description: `I was unable to disconnect ${member.user} from **${member_voice_channel.name}**!`,
+            }, message));
+        } finally {
+            if (disconnected_voice_member) {
                 message.channel.send(new CustomRichEmbed({
-                    title:`@${guildMember.user.tag} has been disconnected!`
-                }));
+                    description: `${message.author} disconnected ${member.user} from ${member_voice_channel}!`,
+                }, message));
                 logAdminCommandsToGuild(message, new CustomRichEmbed({
-                    title:`@${message.author.tag} (${message.author.id}) disconnected @${guildMember.user.tag} (${guildMember.user.id}) from the their voice channel!`
-                }));
-            });
-        }).catch(console.trace);
+                    description: `@${message.author.tag} (${message.author.id}) disconnected @${member.user.tag} (${member.user.id}) from the their voice channel!`,
+                }, message));
+            }
+        }
     },
 });
