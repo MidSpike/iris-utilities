fadd66375cb76a72d62477cd0bd9bd1b637174c8
MidSpike <developer@midspike.com>
2021-04-06 18:01:40 -0400

    fixes

--- src/commands/utilities/poll.js
+++ src/commands/utilities/poll.js
@@ -14,79 +14,86 @@ module.exports = new DisBotCommand({
     name: 'POLL',
     category: `${DisBotCommander.categories.UTILITIES}`,
     weight: 3,
-    description: 'used for creating polls to vote on',
+    description: 'used for creating polls for people to vote on',
     aliases: ['poll'],
+    cooldown: 5_000,
     async executor(Discord, client, message, opts={}) {
         const { discord_command, command_args } = opts;
-        if (command_args.join('').length > 0) {
-            const poll_args = message.content.replace(`${discord_command} `, '').split('\n');
-            const poll_question = poll_args[0];
-            const poll_choices = poll_args.slice(1);
-            if (poll_question && poll_choices.length > 0) {
-                if (poll_choices.length < 10) {
-                    const bot_message = await message.channel.send(new CustomRichEmbed({
-                        title: 'Has created a poll!',
-                        thumbnail: `${bot_cdn_url}/Vote_2020-04-27_0.png`,
-                        fields: [
-                            {
-                                name: 'Poll Question',
-                                value: `${poll_question}`,
-                            }, {
-                                name: 'Poll Choices',
-                                value: `${(await Promise.all(
-                                    poll_choices.map(async (pc, i) => 
-                                        `${(await constructNumberUsingEmoji(i + 1))} — ${pc}`
-                                    )
-                                )).join('\n\n')}`,
-                            },
-                        ],
-                        footer: {
-                            iconURL: `${client.user.displayAvatarURL({ dynamic: true })}`,
-                            text: `${discord_command}`,
+
+        // if (command_args.join('').length === 0) {
+        //     message.channel.send(new CustomRichEmbed({
+        //         title: 'Time to get some results!',
+        //         description: 'You can create a poll by doing the following!',
+        //         fields: [
+        //             {
+        //                 name: 'Example Poll',
+        //                 value: `${'```'}\n${discord_command} Is this a cool feature?\nYes!\nNo!\n${'```'}`,
+        //             },
+        //         ],
+        //     }, message));
+        //     return;
+        // }
+
+        const poll_args = message.content.replace(`${discord_command} `, '').split('\n');
+        const poll_question = poll_args[0];
+        const poll_choices = poll_args.slice(1);
+        if (poll_question && poll_choices.length > 0) {
+            if (poll_choices.length < 10) {
+                const bot_message = await message.channel.send(new CustomRichEmbed({
+                    title: 'Has created a poll!',
+                    thumbnail: `${bot_cdn_url}/Vote_2020-04-27_0.png`,
+                    fields: [
+                        {
+                            name: 'Poll Question',
+                            value: `${poll_question}`,
+                        }, {
+                            name: 'Poll Choices',
+                            value: (await Promise.all(
+                                poll_choices.map(async (pc, i) => 
+                                    `${(await constructNumberUsingEmoji(i + 1))} — ${pc}`
+                                )
+                            )).join('\n\n'),
                         },
-                    }, message));
-                    for (let i = 0; i < poll_choices.length; i++) {
-                        const bot_emoji = await findCustomEmoji(`bot_emoji_${['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'][i]}`);
-                        await bot_message.react(bot_emoji);
-                    }
-                } else {
-                    message.channel.send(new CustomRichEmbed({
-                        color: 0xFFFF00,
-                        title: 'Whoops!',
-                        description: 'Due to the number machine being broken, only 9 answers for a poll are allowed!',
-                        footer: {
-                            iconURL: `${client.user.displayAvatarURL({ dynamic: true })}`,
-                            text: `${discord_command}`,
-                        },
-                    }, message));
+                    ],
+                    footer: {
+                        iconURL: `${client.user.displayAvatarURL({ dynamic: true })}`,
+                        text: `${discord_command}`,
+                    },
+                }, message));
+                for (let i = 0; i < poll_choices.length; i++) {
+                    const bot_emoji = await findCustomEmoji(`bot_emoji_${['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'][i]}`);
+                    await bot_message.react(bot_emoji);
                 }
             } else {
                 message.channel.send(new CustomRichEmbed({
                     color: 0xFFFF00,
                     title: 'Whoops!',
-                    description: [
-                        'That\'s not how you do it!',
-                        'I need a question and choices!',
-                    ].join('\n'),
-                    fields: [
-                        {
-                            name: 'Example Poll',
-                            value: `${'```'}\n${discord_command} Is this a cool feature?\nYes!\nNo!\n${'```'}`,
-                        },
-                    ],
-                    footer: { iconURL: `${client.user.displayAvatarURL({ dynamic: true })}`, text: `${discord_command}` }
+                    description: 'Due to the number machine being broken, only 9 answers for a poll are allowed!',
+                    footer: {
+                        iconURL: `${client.user.displayAvatarURL({ dynamic: true })}`,
+                        text: `${discord_command}`,
+                    },
                 }, message));
             }
         } else {
             message.channel.send(new CustomRichEmbed({
-                title: 'Time to get some results!',
-                description: 'You can create a poll by doing the following!',
+                color: 0xFFFF00,
+                title: 'Whoops!',
+                description: [
+                    'That\'s not how you do it!',
+                    'I need a question and choices!',
+                    '*Also notice how everything is on it\'s own line.*',
+                ].join('\n'),
                 fields: [
                     {
                         name: 'Example Poll',
                         value: `${'```'}\n${discord_command} Is this a cool feature?\nYes!\nNo!\n${'```'}`,
                     },
                 ],
+                footer: {
+                    iconURL: `${client.user.displayAvatarURL({ dynamic: true })}`,
+                    text: `${discord_command}`,
+                },
             }, message));
         }
     },
