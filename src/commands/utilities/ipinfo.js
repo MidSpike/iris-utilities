19f0ee9a9a437c0c22f04c53d9f5753e753c157d
MidSpike <developer@midspike.com>
2021-06-07 20:57:45 -0400

    worked on the ipinfo command

--- src/commands/utilities/ipinfo.js
+++ src/commands/utilities/ipinfo.js
@@ -10,6 +10,30 @@ const { DisBotCommand,
 
 const bot_cdn_url = process.env.BOT_CDN_URL;
 
+const result_key_overrides = {
+    'continent': 'continent name',
+    'continentCode': 'continent code',
+    'country': 'country name',
+    'countryCode': 'country code',
+    'region': 'region code',
+    'regionName': 'region name',
+    'city': 'city name',
+    'district': 'district name',
+    'zip': 'postal code',
+    'lat': 'latitude',
+    'lon': 'longitude',
+    'offset': 'utc offset',
+    'currency': 'currency code',
+    'isp': 'internet service provider',
+    'org': 'organization',
+    'as': 'autonomous system number',
+    'asname': 'autonomous system name',
+    'reverse': 'reverse dns',
+    'mobile': 'cellular connection',
+    'proxy': 'proxy/vpn/tor',
+    'hosting': 'data center',
+};
+
 module.exports = new DisBotCommand({
     name: 'IPINFO',
     category: `${DisBotCommander.categories.UTILITIES}`,
@@ -19,29 +43,37 @@ module.exports = new DisBotCommand({
     async executor(Discord, client, message, opts={}) {
         const { discord_command, command_args } = opts;
 
-        if (command_args[0]) {
-            axios.get(`http://ip-api.com/json/${command_args[0]}?fields=query,city,regionName,country,zip,isp,org`).then(res => {
-                message.channel.send(new CustomRichEmbed({
-                    title: `IP Info`,
-                    description: `Found results for: ${res.data.query}!`,
-                    fields: Object.keys(res.data).map((key) => [key, res.data[key]]).map(props => ({
-                        name: props[0],
-                        value: props[1] || 'not found',
-                    })),
-                }, message));
-            }).catch(console.trace);
-        } else {
+        const search_query = command_args[0]?.trim() ?? '';
+
+        if (search_query.length === 0) {
             message.channel.send(new CustomRichEmbed({
-                title: `You aren't trying to spy on people now, are you?`,
-                description: `With great power, comes great responsibility. Stay legal my friend!`,
+                title: 'You aren\'t trying to spy on people now, are you?',
+                description: 'With great power, comes great responsibility. Stay legal my friend!',
                 thumbnail: `${bot_cdn_url}/encryption-lock-info.jpg`,
                 fields: [
                     {
                         name: 'Command Usage',
                         value: `${'```'}\n${discord_command} IP_ADDRESS_HERE\n${'```'}`,
+                    }, {
+                        name: 'Example Usage',
+                        value: `${'```'}\n${discord_command} 1.1.1.1\n${'```'}`,
                     },
                 ],
             }, message));
+            return;
         }
+
+        /* documentation: https://ip-api.com/docs/api:json */
+        const { data: response_data } = await axios.get(`http://ip-api.com/json/${search_query}?fields=66846719`);
+
+        await message.channel.send(new CustomRichEmbed({
+            title: 'IP Info',
+            description: `Here are the results for \`${search_query}\`!`,
+            fields: Object.entries(response_data).map(([ key, value ]) => ({
+                name: result_key_overrides[key] ?? key,
+                value: `\`${value || 'n/a'}\``,
+                inline: true,
+            })),
+        }, message)).catch(console.warn);
     },
 });
