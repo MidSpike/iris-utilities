19bd415438a7500677b07c77eb10e5c4f63dfb9b
MidSpike <developer@midspike.com>
2021-06-09 08:07:38 -0400

    improved magic8ball command

--- src/commands/fun/magic8ball.js
+++ src/commands/fun/magic8ball.js
@@ -1,12 +1,13 @@
 'use strict';
 
 //#region dependencies
-const { array_random } = require('../../utilities.js');
+const bot_config = require('../../../config.js');
 
-const { CustomRichEmbed } = require('../../libs/CustomRichEmbed.js');
+const { Timer, array_random } = require('../../utilities.js');
+
 const { DisBotCommand,
         DisBotCommander } = require('../../libs/DisBotCommander.js');
-const bot_config = require('../../../config.js');
+const { CustomRichEmbed } = require('../../libs/CustomRichEmbed.js');
 
 const magic8ball_json = require('../../../files/8ball.json');
 //#endregion dependencies
@@ -20,16 +21,41 @@ module.exports = new DisBotCommand({
     description: 'magic8ball',
     aliases: ['magic8ball'],
     async executor(Discord, client, message, opts={}) {
-        const { clean_command_args } = opts;
-        message.channel.send(new CustomRichEmbed({
+        const { discord_command, clean_command_args } = opts;
+
+        const user_text = clean_command_args.join(' ').trim();
+
+        if (user_text.length < 10) {
+            message.channel.send(new CustomRichEmbed({
+                color: 0xFFFF00,
+                title: 'That doesn\'t look right!',
+                description: 'Try typing a sentence after the command!',
+                fields: [
+                    {
+                        name: 'Example Usage',
+                        value: `${'```'}\n${discord_command} Is MineCraft an awesome game?\n${'```'}`,
+                    },
+                ],
+            }, message));
+            return;
+        }
+
+        const bot_message = await message.channel.send(new CustomRichEmbed({
+            title: 'Shaking the magic 8 ball...',
+            image: `${bot_cdn_url}/man-shaking-8-ball.gif`,
+        }, message));
+
+        await Timer(3000); // give the user a chance to view the gif
+
+        await bot_message.edit(new CustomRichEmbed({
             thumbnail: `${bot_cdn_url}/magic-8-ball.webp`,
             title: `${bot_common_name} - 8 Ball Wizard`,
             fields: [
                 {
-                    name: `You said:`,
-                    value: `${'```'}\n${clean_command_args.join(' ')}\n${'```'}`,
+                    name: 'You said',
+                    value: `${'```'}\n${user_text}\n${'```'}`,
                 }, {
-                    name: `I say:`,
+                    name: 'I think that',
                     value: `${'```'}\n${array_random(magic8ball_json)}\n${'```'}`,
                 },
             ],
