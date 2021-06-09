b8f3c73f92bec7658f27be40349b9f71c4474edc
MidSpike <developer@midspike.com>
2021-06-08 20:59:45 -0400

    worked on newyearsball command

--- src/commands/fun/newyearsball.js
+++ src/commands/fun/newyearsball.js
@@ -6,28 +6,33 @@ const path = require('path');
 
 const { Timer } = require('../../utilities.js');
 
-const { CustomRichEmbed } = require('../../libs/CustomRichEmbed.js');
 const { DisBotCommand,
         DisBotCommander } = require('../../libs/DisBotCommander.js');
+const { CustomRichEmbed } = require('../../libs/CustomRichEmbed.js');
 //#endregion dependencies
 
 module.exports = new DisBotCommand({
     name: 'NEWYEARSBALL',
     category: `${DisBotCommander.categories.FUN}`,
     description: 'newyearsball',
-    aliases: ['newyearsball'],
+    aliases: ['newyearsball', 'ball'],
     async executor(Discord, client, message, opts={}) {
         const ball_message = await message.channel.send(new CustomRichEmbed({
-            title: 'Preparing New Years Ball',
+            title: 'Preparing New Years Ball...',
         }, message));
+
+        await Timer(500); // prevent api abuse
+
         for (let ball_number = 1; ball_number <= 11; ball_number++) {
             const ball_path = path.join(process.cwd(), `./files/new_years_ball/ball_${ball_number}.txt`);
             const ball_art = fs.readFileSync(ball_path).toString();
+
             await ball_message.edit(new CustomRichEmbed({
-                title: `Ball ${ball_number}`,
+                title: 'New Years Ball',
                 description: `${'```'}\n${ball_art}\n${'```'}`,
-            }));
-            await Timer(1000);
+            }, message));
+
+            await Timer(2000); // prevent api abuse
         }
     },
 });
