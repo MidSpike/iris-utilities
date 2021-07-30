807c53e3373917ed95db52129f2579c7b92dc6ff
MidSpike <developer@midspike.com>
2021-07-30 11:08:53 -0400

    updates

--- src/commands/help.js
+++ src/commands/help.js
@@ -4,27 +4,37 @@
 
 const Discord = require('discord.js');
 
-const { ClientCommand, ClientCommandHandlerOptions } = require('../common/client_commands');
+const { ClientCommand, ClientCommandHandler } = require('../common/client_commands');
 
 //------------------------------------------------------------//
 
 module.exports = new ClientCommand({
     name: 'help',
-    aliases: ['help', 'h'],
     description: 'Displays a list of commands.',
+    options: [
+        {
+            type: 'INTEGER',
+            name: 'page-number',
+            description: 'the page number',
+            required: false,
+        },
+    ],
     permissions: [
+        Discord.Permissions.FLAGS.VIEW_CHANNEL,
         Discord.Permissions.FLAGS.SEND_MESSAGES,
     ],
     contexts: [
         'ALL_CHANNELS',
     ],
-    /**
-     * @param {Discord.Message} message
-     * @param {ClientCommandHandlerOptions} opts
-     */
-    async handler(message, opts) {
-        message.reply({
+    /** @type {ClientCommandHandler} */
+    async handler(discord_client, command_interaction) {
+        command_interaction.reply({
             content: 'this is going to be the help command',
+            embeds: [
+                {
+                    title: 'This is a test!',
+                },
+            ],
         });
     },
 });
