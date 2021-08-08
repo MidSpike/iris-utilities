842a5bbb4b633bc99fed79946f91c8558cfac513
MidSpike <developer@midspike.com>
2021-08-08 17:39:23 -0400

    changes

--- src/events/ready.js
+++ src/events/ready.js
@@ -11,7 +11,7 @@ const { GuildConfigsManager } = require('../common/guild_configs');
 //------------------------------------------------------------//
 
 async function updateAllGuildConfigs(discord_client) {
-    console.log(`<DC S#(${discord_client.shard.id})> updating all guild configs...`);
+    console.info(`<DC S#(${discord_client.shard.id})> updating all guild configs...`);
 
     for (const [ guild_id ] of discord_client.guilds.cache) {
         await GuildConfigsManager.update(guild_id, {});
@@ -20,8 +20,8 @@ async function updateAllGuildConfigs(discord_client) {
     console.success(`<DC S#(${discord_client.shard.id})> updated all guild configs.`);
 }
 
-async function registerDiscordClientCommands(discord_client) {
-    console.log(`<DC S#(${discord_client.shard.ids.join(', ')})> registering commands...`);
+async function registerCommands(discord_client) {
+    console.info(`<DC S#(${discord_client.shard.ids.join(', ')})> registering commands...`);
 
     const path_to_command_files = path.join(process.cwd(), 'src', 'commands');
     const client_command_file_names = recursiveReadDirectory(path_to_command_files);
@@ -29,7 +29,7 @@ async function registerDiscordClientCommands(discord_client) {
     for (const client_command_file_name of client_command_file_names) {
         const client_command_file_path = path.join(path_to_command_files, client_command_file_name);
 
-        console.log(`<DC S#(${discord_client.shard.ids.join(', ')})> loading command...`, { client_command_file_path });
+        console.info(`<DC S#(${discord_client.shard.ids.join(', ')})> loading command...`, { client_command_file_path });
 
         try {
             const client_command = require(client_command_file_path);
@@ -48,6 +48,25 @@ async function registerDiscordClientCommands(discord_client) {
     }
 }
 
+async function unregisterDeletedCommands(discord_client) {
+    console.info(`<DC S#(${discord_client.shard.id})> un-registering deleted commands...`);
+
+    for (const guild of discord_client.guilds.cache.values()) {
+        console.info(`<DC S#(${discord_client.shard.id})> un-registering deleted commands from guild: ${guild.id};`);
+
+        const guild_commands = await guild.commands.fetch();
+        for (const guild_command of guild_commands.values()) {
+            const client_command = ClientCommandManager.commands.get(guild_command.name);
+            if (client_command) continue;
+
+            console.info(`<DC S#(${discord_client.shard.id})> un-registering deleted command: ${guild_command.name}; from guild: ${guild.id};`);
+            await guild.commands.delete(guild_command.id);
+        }
+    }
+
+    console.success(`<DC S#(${discord_client.shard.id})> un-registered all deleted commands.`);
+}
+
 //------------------------------------------------------------//
 
 module.exports = {
@@ -59,6 +78,9 @@ module.exports = {
         updateAllGuildConfigs(discord_client);
 
         /* register all commands */
-        registerDiscordClientCommands(discord_client);
+        registerCommands(discord_client);
+
+        /* un-register deleted commands */
+        unregisterDeletedCommands(discord_client);
     },
 };
