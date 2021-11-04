7438df841573c83143cdb5425d97fd40981a84fc
MidSpike <developer@midspike.com>
2021-11-03 21:48:09 -0400

    stuff

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,10 +2,7 @@
 
 //------------------------------------------------------------//
 
-const path = require('path');
-const recursiveReadDirectory = require('recursive-read-directory');
-
-const { ClientCommand, ClientCommandManager } = require('../common/app/client_commands');
+const { ClientInteractionManager } = require('../common/app/client_interactions');
 const { GuildConfigsManager } = require('../common/app/guild_configs');
 
 //------------------------------------------------------------//
@@ -20,53 +17,6 @@ async function updateAllGuildConfigs(discord_client) {
     console.success(`<DC S#(${discord_client.shard.id})> updated all guild configs.`);
 }
 
-async function registerCommands(discord_client) {
-    console.info(`<DC S#(${discord_client.shard.ids.join(', ')})> registering commands...`);
-
-    const path_to_command_files = path.join(process.cwd(), 'src', 'commands');
-    const client_command_file_names = recursiveReadDirectory(path_to_command_files);
-
-    for (const client_command_file_name of client_command_file_names) {
-        const client_command_file_path = path.join(path_to_command_files, client_command_file_name);
-
-        console.info(`<DC S#(${discord_client.shard.ids.join(', ')})> loading command...`, { client_command_file_path });
-
-        try {
-            const client_command = require(client_command_file_path);
-            if (!(client_command instanceof ClientCommand)) throw new Error('client_command is not an instance of ClientCommand');
-
-            await ClientCommandManager.loadCommand(client_command);
-        } catch (error) {
-            console.trace('unable to register global command:', client_command_file_path, error);
-            continue;
-        }
-    }
-
-    for (const [ guild_id ] of discord_client.guilds.cache) {
-        console.info(`<DC S#(${discord_client.shard.ids.join(', ')})> registering all commands to guild: ${guild_id};`);
-        await ClientCommandManager.registerAllCommandsToGuild(discord_client, guild_id);
-    }
-}
-
-async function unregisterDeletedCommands(discord_client) {
-    console.info(`<DC S#(${discord_client.shard.id})> un-registering deleted commands...`);
-
-    for (const guild of discord_client.guilds.cache.values()) {
-        console.info(`<DC S#(${discord_client.shard.id})> un-registering deleted commands from guild: ${guild.id};`);
-
-        const guild_commands = await guild.commands.fetch();
-        for (const guild_command of guild_commands.values()) {
-            const client_command = ClientCommandManager.commands.get(guild_command.name);
-            if (client_command) continue;
-
-            console.info(`<DC S#(${discord_client.shard.id})> un-registering deleted command: ${guild_command.name}; from guild: ${guild.id};`);
-            await guild.commands.delete(guild_command.id);
-        }
-    }
-
-    console.success(`<DC S#(${discord_client.shard.id})> un-registered all deleted commands.`);
-}
-
 //------------------------------------------------------------//
 
 module.exports = {
@@ -77,10 +27,7 @@ module.exports = {
         /* update all guild configs */
         updateAllGuildConfigs(discord_client);
 
-        /* register all commands */
-        registerCommands(discord_client);
-
-        /* un-register deleted commands */
-        unregisterDeletedCommands(discord_client);
+        /* load all client interactions */
+        ClientInteractionManager.loadClientInteractions();
     },
 };
---
+++ src/events/speech.js
@@ -0,0 +1,123 @@
+'use strict';
+
+//------------------------------------------------------------//
+
+const Discord = require('discord.js');
+
+const { CustomEmbed } = require('../common/app/message');
+const { AudioManager, VolumeManager } = require('../common/app/audio');
+
+//------------------------------------------------------------//
+
+module.exports = {
+    name: 'speech',
+    /**
+     * @param {Discord.Client} discord_client
+     * @param {{
+     *  content: string?,
+     *  channel: Discord.VoiceChannel?,
+     *  author: Discord.User?,
+     * }} msg
+     */
+    async handler(discord_client, msg) {
+        if (!msg?.content?.length) return;
+
+        console.log({
+            voice_recognition: msg.content,
+        });
+
+        const voice_command_activation_regex = /^(\b(hey|a|play|yo|yellow|ok|oi)\b\s)?\b(Ir(i?)s(h?)|Discord|Ziggy|Alexa|Google|Siri|Bixby|Cortana|Tesla)\b/gi;
+
+        if (!voice_command_activation_regex.test(msg.content)) return;
+
+        console.log({
+            voice_message: {
+                content: msg.content,
+                author_id: msg.author.id,
+                voice_channel_id: msg.channel.id,
+            },
+        });
+
+        /** @type {string} */
+        const user_input = msg.content.replace(voice_command_activation_regex, '').toLowerCase().trim();
+        const user_input_args = user_input.split(/\s+/gi);
+
+        const voice_command_name = user_input_args[0] ?? '';
+        const voice_command_args = user_input_args.slice(1) ?? [];
+
+        if (!voice_command_name.length) return;
+
+        const voice_command_text_channel = discord_client.channels.cache.get('810555581177397248');
+
+        await voice_command_text_channel.send({
+            embeds: [
+                new CustomEmbed({
+                    title: 'Voice Command',
+                    description: `${msg.author.username} said:\`\`\`\n${voice_command_name} ${voice_command_args.join(' ')}\n\`\`\``,
+                }),
+            ],
+        }).catch(console.warn);
+
+        switch (voice_command_name) {
+            case 'by': // this is needed b/c google sometimes picks up 'by' instead of 'play'
+            case 'hey': // this is needed b/c google sometimes picks up 'hey' instead of 'play'
+            case 'play': {
+                const queue = await AudioManager.createQueue(discord_client, msg.channel.guild.id, {
+                    user: msg.author,
+                    channel: voice_command_text_channel,
+                });
+
+                const search_result = await queue.player.search(`${voice_command_args.join(' ')}`, {
+                    requestedBy: msg.author,
+                }).catch(() => {});
+
+                if (!search_result?.tracks?.length) return;
+
+                if (!queue.connection) {
+                    await queue.connect(msg.channel.id);
+                }
+
+                queue.play(search_result.tracks[0]);
+
+                break;
+            }
+
+            case 'vol': // this is needed b/c google sometimes picks up 'vol' instead of 'volume'
+            case 'volume': {
+                const volume_input = Number.parseInt(voice_command_args[0], 10);
+
+                if (Number.isNaN(volume_input)) return;
+
+                const minimum_allowed_volume = 0;
+                const maximum_allowed_volume = 100;
+                const volume_level = Math.max(minimum_allowed_volume, Math.min(volume_input, maximum_allowed_volume));
+
+                const queue = await AudioManager.createQueue(discord_client, msg.channel.guild.id);
+                queue.setVolume(VolumeManager.scaleVolume(VolumeManager.lockToNearestMultipleOf(volume_level, 5)));
+
+                break;
+            }
+
+            case 'skit': // this is needed b/c google sometimes picks up 'skit' instead of 'skip'
+            case 'skin': // this is needed b/c google sometimes picks up 'skin' instead of 'skip'
+            case 'skip': {
+                const queue = await AudioManager.createQueue(discord_client, msg.channel.guild.id);
+                queue.skip();
+
+                break;
+            }
+
+            case 'star': // this is needed b/c google sometimes picks up 'star' instead of 'stop'
+            case 'stop': {
+                const queue = await AudioManager.createQueue(discord_client, msg.channel.guild.id);
+                queue.stop();
+
+                break;
+            }
+
+            default: {
+                break;
+            }
+        }
+    },
+};
