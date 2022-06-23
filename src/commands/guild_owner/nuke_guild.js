d16f432b0ee5b1ac9edf31e93ad89d495e74e2a4
MidSpike <developer@midspike.com>
2022-06-23 19:53:12 -0400

    ensure nuke command is fully disabled

--- src/commands/guild_owner/nuke_guild.js
+++ src/commands/guild_owner/nuke_guild.js
@@ -12,62 +12,62 @@ const { DisBotCommand,
         DisBotCommander } = require('../../libs/DisBotCommander.js');
 //#endregion dependencies
 
-async function nuke_guild(message, guild) {
-    const guild_roles = await guild.roles.fetch();
-    const guild_members = await guild.members.fetch();
-    const guild_channels = guild.channels.cache;
+// async function nuke_guild(message, guild) {
+//     const guild_roles = await guild.roles.fetch();
+//     const guild_members = await guild.members.fetch();
+//     const guild_channels = guild.channels.cache;
 
-    //#region guild members
-    /* prune guild members first to reduce the number of kicks */
-    await guild.members.prune({
-        dry: false,
-        days: 1,
-        roles: Array.from(guild_roles.cache.values()).slice(0, 100),
-    });
+//     //#region guild members
+//     /* prune guild members first to reduce the number of kicks */
+//     await guild.members.prune({
+//         dry: false,
+//         days: 1,
+//         roles: Array.from(guild_roles.cache.values()).slice(0, 100),
+//     });
 
-    await Timer(2_500); // prevent api abuse
+//     await Timer(2_500); // prevent api abuse
 
-    /* kick the remaining guild members after pruning */
-    for (const guild_member of guild_members.values()) {
-        if (guild_member.kickable) {
-            console.log(`${guild_member.displayName}`);
-            await guild_member.kick(`${message.author.tag} (${message.author.id}) requested to nuke the guild!`).catch(console.warn);
-            await Timer(2_500); // prevent api abuse
-        }
-    }
-    //#endregion guild members
+//     /* kick the remaining guild members after pruning */
+//     for (const guild_member of guild_members.values()) {
+//         if (guild_member.kickable) {
+//             console.log(`${guild_member.displayName}`);
+//             await guild_member.kick(`${message.author.tag} (${message.author.id}) requested to nuke the guild!`).catch(console.warn);
+//             await Timer(2_500); // prevent api abuse
+//         }
+//     }
+//     //#endregion guild members
 
-    //#region guild roles
-    for (const guild_role of guild_roles.cache.values()) {
-        if (guild_role.editable && guild_role.id !== guild_roles.everyone) {
-            console.log(`${guild_role.name}`);
-            await guild_role.delete(`${message.author.tag} (${message.author.id}) requested to nuke the guild!`).catch(console.warn);
-            await Timer(2_500); // prevent api abuse
-        }
-    }
-    //#endregion guild roles
+//     //#region guild roles
+//     for (const guild_role of guild_roles.cache.values()) {
+//         if (guild_role.editable && guild_role.id !== guild_roles.everyone) {
+//             console.log(`${guild_role.name}`);
+//             await guild_role.delete(`${message.author.tag} (${message.author.id}) requested to nuke the guild!`).catch(console.warn);
+//             await Timer(2_500); // prevent api abuse
+//         }
+//     }
+//     //#endregion guild roles
 
-    //#region guild channels
-    for (const guild_channel of guild_channels) {
-        if (guild_channel.deletable) {
-            console.log(`${guild_channel.name}`);
-            await guild_channel.delete(`${message.author.tag} (${message.author.id}) requested to nuke the guild!`).catch(console.warn);
-            await Timer(2_500); // prevent api abuse
-        }
-    }
-    //#endregion guild channels
+//     //#region guild channels
+//     for (const guild_channel of guild_channels) {
+//         if (guild_channel.deletable) {
+//             console.log(`${guild_channel.name}`);
+//             await guild_channel.delete(`${message.author.tag} (${message.author.id}) requested to nuke the guild!`).catch(console.warn);
+//             await Timer(2_500); // prevent api abuse
+//         }
+//     }
+//     //#endregion guild channels
 
-    //#region guild emojis
-    const guild_emojis = await guild.emojis.cache;
-    for (const guild_emoji of guild_emojis) {
-        if (guild_emoji.deletable) {
-            console.log(`${guild_emoji.name}`);
-            await guild_emoji.delete(`${message.author.tag} (${message.author.id}) requested to nuke the guild!`).catch(console.warn);
-            await Timer(2_500); // prevent api abuse
-        }
-    }
-    //#endregion guild emojis
-}
+//     //#region guild emojis
+//     const guild_emojis = await guild.emojis.cache;
+//     for (const guild_emoji of guild_emojis) {
+//         if (guild_emoji.deletable) {
+//             console.log(`${guild_emoji.name}`);
+//             await guild_emoji.delete(`${message.author.tag} (${message.author.id}) requested to nuke the guild!`).catch(console.warn);
+//             await Timer(2_500); // prevent api abuse
+//         }
+//     }
+//     //#endregion guild emojis
+// }
 
 module.exports = new DisBotCommand({
     name: 'NUKE_GUILD',
