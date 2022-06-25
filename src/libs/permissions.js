7f32bac658a4cd8eb504f4b23ae0c48b5fdc8e99
MidSpike <developer@midspike.com>
2022-06-24 20:02:40 -0400

    spell check

--- src/libs/permissions.js
+++ src/libs/permissions.js
@@ -26,7 +26,7 @@ function botHasPermissionsInGuild(message, required_permissions=[Discord.Permiss
             description: [
                 `${bot_common_name} is missing the following permission(s):`,
                 `\n${'```'}\n${required_permissions.join('\n')}\n${'```'}`,
-                `You cannot perform this command when I don\'t have teh required permission(s)!`,
+                `You cannot perform this command when I don\'t have the required permission(s)!`,
             ].join(''),
         }, message));
         return false;
@@ -48,9 +48,8 @@ const isSuperPersonAllowed = (super_person, permission_flag) => {
             const allowed_permission_flag = super_person?.allowed_permissions?.includes(permission_flag) ?? false;
             const denied_all_permissions = super_person?.denied_permissions?.includes('*') ?? false;
             const denied_permission_flag = super_person?.denied_permissions?.includes(permission_flag) ?? false;
-            const has_all_permissions = allowed_all_permissions && !denied_all_permissions;
-            const has_this_permission = allowed_permission_flag && !denied_permission_flag;
-            return (has_all_permissions || has_this_permission);
+            const has_permission = !denied_all_permissions && !denied_permission_flag && (allowed_all_permissions || allowed_permission_flag);
+            return has_permission;
         }
     } else {
         return false;
