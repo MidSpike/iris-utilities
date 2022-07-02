44e1deae9b15f30a6622ce3f2fe1e1402af5869c
MidSpike <developer@midspike.com>
2022-07-01 20:02:55 -0400

    changed how permissions are handled

--- src/common/app/permissions.ts
+++ src/common/app/permissions.ts
@@ -21,21 +21,35 @@ if (!db_super_people_collection_name?.length) throw new TypeError('MONGO_SUPER_P
 /**
  * @param member the member that should be above the other member
  * @param other_member the other member that should be below the member
+ * @param compare_admins when true, if both members have 'Administrator' permission, factor hierarchy into consideration
  */
 export function isMemberAboveOtherMember(
     member: Discord.GuildMember,
     other_member: Discord.GuildMember,
+    compare_admins: boolean = true,
 ): boolean {
-    if (member.id === other_member.id) return false;
+    // check if both members are from the same guild
     if (member.guild.id !== other_member.guild.id) return false;
 
+    // check if both members are the same member
+    if (member.id === other_member.id) return false;
+
+    // check if either member is the guild owner
     if (member.id === member.guild.ownerId) return true;
     if (other_member.id === member.guild.ownerId) return false;
+
+    // when `compare_admins === false`, treat members with 'Administrator' as equal in hierarchy
+    if (
+        !compare_admins &&
+        member.permissions.has(Discord.PermissionFlagsBits.Administrator) &&
+        other_member.permissions.has(Discord.PermissionFlagsBits.Administrator)
+    ) return false;
 
     const member_highest_role = member.roles.highest;
     const other_member_highest_role = other_member.roles.highest;
 
     if (member_highest_role.id === other_member_highest_role.id) return false;
+
     if (member_highest_role.comparePositionTo(other_member_highest_role) > 0) return true;
 
     return false;
@@ -46,8 +60,10 @@ export function isMemberAboveOtherMember(
  * - the member is the owner of the guild
  * - the member has Administrator permissions
  * - the member is a super person (an admin for this bot)
+ * @param member the member to check
+ * @param permission_flag_bit the permission to check for
  */
-export async function doesMemberPossessPermissionFlagBit(
+export async function doesMemberHavePermission(
     member: Discord.GuildMember,
     permission_flag_bit: bigint,
 ): Promise<boolean> {
