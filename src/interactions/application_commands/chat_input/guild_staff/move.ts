75f5c6dac222557d773c8097b6223f2cb4bf3bd6
MidSpike <developer@midspike.com>
2022-07-10 19:56:02 -0400

    added move command

---
+++ src/interactions/application_commands/chat_input/guild_staff/move.ts
@@ -0,0 +1,174 @@
+//------------------------------------------------------------//
+//        Copyright (c) MidSpike. All rights reserved.        //
+//------------------------------------------------------------//
+
+import * as Discord from 'discord.js';
+
+import { CustomEmbed } from '@root/common/app/message';
+
+import { ClientCommandHelper, ClientInteraction } from '@root/common/app/client_interactions';
+
+import { doesMemberHavePermission, isMemberAboveOtherMember } from '@root/common/app/permissions';
+
+//------------------------------------------------------------//
+
+export default new ClientInteraction<Discord.ChatInputApplicationCommandData>({
+    identifier: 'move',
+    type: Discord.InteractionType.ApplicationCommand,
+    data: {
+        type: Discord.ApplicationCommandType.ChatInput,
+        description: 'moves a user to a different voice channel',
+        options: [
+            {
+                type: Discord.ApplicationCommandOptionType.User,
+                name: 'member',
+                description: 'the guild member to move',
+                required: true,
+            }, {
+                type: Discord.ApplicationCommandOptionType.Channel,
+                name: 'channel',
+                description: 'the voice channel to move the member to',
+                required: true,
+            }, {
+                type: Discord.ApplicationCommandOptionType.String,
+                name: 'reason',
+                description: 'the reason for the move',
+                required: false,
+            },
+        ],
+    },
+    metadata: {
+        allowed_execution_environment: ClientCommandHelper.execution_environments.GUILD_ONLY,
+        required_user_access_level: ClientCommandHelper.access_levels.GUILD_STAFF,
+        required_bot_permissions: [
+            Discord.PermissionFlagsBits.ViewChannel,
+            Discord.PermissionFlagsBits.SendMessages,
+            Discord.PermissionFlagsBits.MoveMembers,
+        ],
+        command_category: ClientCommandHelper.categories.GUILD_STAFF,
+    },
+    async handler(discord_client, interaction) {
+        if (!interaction.isChatInputCommand()) return;
+        if (!interaction.inCachedGuild()) return;
+        if (!interaction.channel) return;
+
+        await interaction.deferReply({ ephemeral: false });
+
+        const is_user_allowed_to_mute = await doesMemberHavePermission(interaction.member, Discord.PermissionFlagsBits.MoveMembers);
+        if (!is_user_allowed_to_mute) {
+            await interaction.editReply({
+                embeds: [
+                    CustomEmbed.from({
+                        color: CustomEmbed.colors.RED,
+                        description: `${interaction.user}, you do not have permission to move members`,
+                    }),
+                ],
+            });
+
+            return;
+        }
+
+        const member = interaction.options.getMember('member');
+        const channel = interaction.options.getChannel('channel', true);
+        const reason = Discord.escapeMarkdown(
+            interaction.options.getString('reason', false) || 'no reason was provided'
+        );
+
+        if (!member) {
+            await interaction.editReply({
+                embeds: [
+                    CustomEmbed.from({
+                        color: CustomEmbed.colors.YELLOW,
+                        description: `${interaction.user}, you must specify a valid user to mute!`,
+                    }),
+                ],
+            });
+
+            return;
+        }
+
+        if (!channel || !channel.isVoiceBased()) {
+            await interaction.editReply({
+                embeds: [
+                    CustomEmbed.from({
+                        color: CustomEmbed.colors.YELLOW,
+                        description: `${interaction.user}, you must specify a valid voice channel!`,
+                    }),
+                ],
+            });
+
+            return;
+        }
+
+        const bot_member = await interaction.guild.members.fetch(discord_client.user.id);
+        if (!isMemberAboveOtherMember(bot_member, member)) {
+            await interaction.editReply({
+                embeds: [
+                    CustomEmbed.from({
+                        color: CustomEmbed.colors.YELLOW,
+                        description: `${interaction.user}, I\'m not allowed to move ${member}!`,
+                    }),
+                ],
+            });
+
+            return;
+        }
+
+        if (!isMemberAboveOtherMember(interaction.member, member)) {
+            await interaction.editReply({
+                embeds: [
+                    CustomEmbed.from({
+                        color: CustomEmbed.colors.YELLOW,
+                        description: `${interaction.user}, you are not allowed to move ${member}!`,
+                    }),
+                ],
+            });
+
+            return;
+        }
+
+        try {
+            await member.voice.setChannel(channel, `Moved by ${interaction.user} for ${reason}`);
+        } catch (error) {
+            await interaction.editReply({
+                embeds: [
+                    CustomEmbed.from({
+                        color: CustomEmbed.colors.RED,
+                        description: `${interaction.user}, failed to move ${member}!`,
+                        fields: [
+                            {
+                                name: 'Error Message',
+                                value: [
+                                    '\`\`\`',
+                                    `${error}`,
+                                    '\`\`\`',
+                                ].join('\n'),
+                            },
+                        ],
+                    }),
+                ],
+            });
+
+            return;
+        }
+
+        await interaction.editReply({
+            embeds: [
+                CustomEmbed.from({
+                    color: CustomEmbed.colors.GREEN,
+                    description: `${interaction.user}, successfully moved ${member}!`,
+                    fields: [
+                        {
+                            name: 'Reason',
+                            value: [
+                                '\`\`\`',
+                                `${reason}`,
+                                '\`\`\`',
+                            ].join('\n'),
+                        },
+                    ],
+                }),
+            ],
+        });
+    },
+});
