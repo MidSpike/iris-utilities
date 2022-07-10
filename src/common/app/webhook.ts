385297afd980fdc50062994f9cb304f8358f3bfa
MidSpike <developer@midspike.com>
2022-07-10 19:05:19 -0400

    refactor

--- src/common/app/webhook.ts
+++ src/common/app/webhook.ts
@@ -11,17 +11,11 @@ import * as Discord from 'discord.js';
 export async function sendWebhookMessage(
     webhook_url: string,
     message: Partial<Discord.APIMessage>,
-): Promise<boolean> {
-    try {
-        await axios({
-            method: 'POST',
-            url: webhook_url,
-            data: message,
-            validateStatus: (status_code) => status_code >= 200 && status_code < 300,
-        });
-    } catch {
-        return false;
-    }
-
-    return true;
+): Promise<void> {
+    await axios({
+        method: 'POST',
+        url: webhook_url,
+        data: message,
+        validateStatus: (status_code) => status_code >= 200 && status_code < 300,
+    });
 }
