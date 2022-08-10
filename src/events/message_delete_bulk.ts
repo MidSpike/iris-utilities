cb55b1543e0df1cf50243e139a0239fe24de95c2
MidSpike <developer@midspike.com>
2022-08-09 21:31:46 -0400

    added new logging events

---
+++ src/events/message_delete_bulk.ts
@@ -0,0 +1,49 @@
+//------------------------------------------------------------//
+//        Copyright (c) MidSpike. All rights reserved.        //
+//------------------------------------------------------------//
+
+import { ClientEventExport, GuildConfigLoggingChannels } from '@root/types/index';
+
+import * as Discord from 'discord.js';
+
+import { CustomEmbed } from '@root/common/app/message';
+
+import { guildLogger } from '@root/common/app/loggers/loggers';
+
+//------------------------------------------------------------//
+
+const event_name = Discord.Events.MessageBulkDelete;
+export default {
+    name: event_name,
+    async handler(
+        discord_client,
+        messages,
+        channel,
+    ) {
+        if (!discord_client.isReady()) return;
+
+        if (channel.isDMBased()) return; // ignore direct messages
+        if (!channel.guild) return; // ignore direct messages
+
+        const current_timestamp = `${Date.now()}`.slice(0, -3);
+
+        guildLogger(
+            channel.guild,
+            GuildConfigLoggingChannels.MESSAGE_DELETED,
+            {
+                embeds: [
+                    CustomEmbed.from({
+                        color: CustomEmbed.colors.RED,
+                        description: `${messages.size} messages were deleted in ${channel}.`,
+                        fields: [
+                            {
+                                name: 'Deleted On',
+                                value: `<t:${current_timestamp}:f> (<t:${current_timestamp}:R>)`,
+                            },
+                        ],
+                    }).toJSON(),
+                ],
+            }
+        );
+    },
+} as ClientEventExport<typeof event_name>;
