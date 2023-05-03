04c1eac2765c7c2da1328330fe267837509ee3b0
MidSpike <developer@midspike.com>
2023-05-02 20:42:28 -0400

    hot fix

--- src/common/app/message.ts
+++ src/common/app/message.ts
@@ -71,7 +71,10 @@ export class CustomEmbed {
     static from(options: Discord.APIEmbed): Discord.EmbedBuilder {
         options.color ??= this.Colors.Brand;
 
-        return Discord.EmbedBuilder.from(options);
+        return new Discord.EmbedBuilder(options);
+
+        /** @todo re-implement this when discord.js v14.11.x is released */
+        // return Discord.EmbedBuilder.from(options);
     }
 }
 
