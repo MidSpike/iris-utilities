58f8484ea4c3da61412935b42da8ce45ade145b5
MidSpike <developer@midspike.com>
2023-06-25 11:45:11 -0400

    updates

--- src/index.ts
+++ src/index.ts
@@ -66,7 +66,7 @@ console.log('<SM> spawning shards...');
 sharding_manager.spawn({
     amount: 'auto', // how many shards to spawn
     delay: 5_000, // how long to wait between shards spawning
-    timeout: 120_000, // how long to wait for a shard to become ready before continuing to the next shard
+    timeout: 2 * 60_000, // how long to wait for a shard to become ready before continuing to the next shard
 });
 
 //------------------------------------------------------------//
@@ -75,7 +75,7 @@ setInterval(() => {
     console.log('<SM> respawning all shards...');
     sharding_manager.respawnAll({
         shardDelay: 5_000, // how long to wait between shards spawning
-        respawnDelay: 1 * 60_000, // how long to wait between killing a shard and restarting it
+        respawnDelay: 5_000, // how long to wait between killing a shard and restarting it
         timeout: 2 * 60_000, // how long to wait for a shard to become ready before continuing to the next shard
     });
 }, 24 * 60 * 60_000); // restart all shards every interval
